a5d8e4d4ca45d09a8bb7bd3caa1a79b382b0cbdc
PetrKrysl <pkrysl@ucsd.edu>
2017-06-30 10:29:50 -0700

    Increasing coverage with the static deformation test

--- examples/deformation_linear/statics/3-d/multimaterial_beam_algo.jl
+++ examples/deformation_linear/statics/3-d/multimaterial_beam_algo.jl
@@ -1,87 +1,90 @@
-using JFinEALE
-using JFinEALE.AlgoDeformationLinearModule
-using JFinEALE.MeshExportModule
+using FinEtools
+using FinEtools.AlgoDeforLinearModule
+using FinEtools.MeshExportModule
 
 println("""
-Multi-material beam. Rubber-like and metal-like halfs, 
+Multi-material beam. Rubber-like and metal-like halfs,
 clamped, with shear traction at free end.
 """)
 function  multimaterial_beam()
-    E1=0.29e3;
-    nu1=0.49;
-    E2=0.4e4;
-    nu2=0.3;
-    W=4.1;
-    L=12.;
-    t=6.5;
-    nl=2; nt=1; nw=1; ref=9;
-    p=  200./W/t;
-    #  Loading in the Z direction
-    loadv=[0;0;p];dir=3; 
-    tolerance =t/1000;
-    
-    fens,fes =H20block(L,W,t, nl*ref,nw*ref,nt*ref)
+  E1 = 0.29e3;
+  nu1 = 0.49;
+  E2 = 0.4e4;
+  nu2 = 0.3;
+  W = 4.1;
+  L = 12.;
+  t = 6.5;
+  nl = 2; nt = 1; nw = 1; ref = 9;
+  p =   200./W/t;
+  #  Loading in the Z direction
+  loadv = [0;0;p]; dir = 3;
+  tolerance  = t/1000;
 
-    # Clamped end of the beam
-    l1 =selectnode(fens; box=[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
-    essential1=dmake(node_list=l1,component=1:3,displacement=0.0)
+  fens,fes  = H20block(L,W,t, nl*ref,nw*ref,nt*ref)
 
-    # Traction on the opposite edge
-    boundaryfes =  meshboundary(fes);
-    Toplist  =selectelem(fens,boundaryfes, box= [L L -Inf Inf -Inf Inf], inflate=  tolerance);
-    el1femm =  FEMMBase(subset(boundaryfes,Toplist), GaussRule(order=2,dim=2))
-    flux1=dmake(femm=el1femm,traction_vector=loadv)
+  # Clamped end of the beam
+  l1  = selectnode(fens; box = [0 0 -Inf Inf -Inf Inf], inflate  =  tolerance)
+  e1 = FDataDict("node_list"=>l1, "component"=>1, "displacement"=>0.0)
+  e2 = FDataDict("node_list"=>l1, "component"=>2, "displacement"=>0.0)
+  e3 = FDataDict("node_list"=>l1, "component"=>3, "displacement"=>0.0)
 
-    r1list  =selectelem(fens,fes, box= [0 L/2. -Inf Inf -Inf Inf], inflate=  tolerance);
-    r2list  =selectelem(fens,fes, box= [L/2. L -Inf Inf -Inf Inf], inflate=  tolerance);
-    
-    # Make region 1
-    p1=PropertyDeformationLinearIso(E1,nu1)
-    region1=dmake(fes=subset(fes,r1list), property=p1, integration_rule=GaussRule(order=2,dim=3))
+  # Traction on the opposite edge
+  boundaryfes  =   meshboundary(fes);
+  Toplist   = selectelem(fens,boundaryfes, box =  [L L -Inf Inf -Inf Inf], inflate =   tolerance);
+  el1femm  =   FEMMBase(GeoD(subset(boundaryfes,Toplist), GaussRule(2, 2)))
+  flux1 = FDataDict("femm"=>el1femm, "traction_vector"=>loadv)
 
-    # Make region 2
-    p2=PropertyDeformationLinearIso(E2,nu2)
-    region2=dmake(fes=subset(fes,r2list), property=p2, integration_rule=GaussRule(order=2,dim=3))
+  r1list   = selectelem(fens,fes, box =  [0 L/2. -Inf Inf -Inf Inf], inflate =   tolerance);
+  r2list   = selectelem(fens,fes, box =  [L/2. L -Inf Inf -Inf Inf], inflate =   tolerance);
 
-    # Model reduction type
-    mr=DeformationModelReduction3D
+  # Model reduction type
+  MR = DeforModelRed3D
 
-    # Make model data
-    modeldata= dmake(modelreduction=mr,
-                     fens= fens,region=[region1 region2],
-                     boundary_conditions=dmake(traction=[flux1],essential=[essential1]));
+  # Make region 1
+  region1 = FDataDict("femm"=>FEMMDeforLinear(MR,
+      GeoD(subset(fes,r1list), GaussRule(3,2)),
+      MatDeforElastIso(MR, 0.0, E1, nu1, 0.0)))
 
-    # Call the solver
-    modeldata=JFinEALE.AlgoDeformationLinearModule.linearstatics(modeldata)
-    geom=modeldata["geom"]
-    u=modeldata["u"]
+  # Make region 2
+  region2 = FDataDict("femm"=>FEMMDeforLinear(MR,
+      GeoD(subset(fes,r2list), GaussRule(3,2)),
+      MatDeforElastIso(MR, 0.0, E2, nu2, 0.0)))
 
-    # Write out mesh with displacements
-    dadd!(modeldata,postprocessing=dmake(file=  "multimaterial_beam"))
-    modeldata=JFinEALE.AlgoDeformationLinearModule.exportdeformation(modeldata)
+  # Make model data
+  modeldata =  FDataDict(
+  "fens"=> fens, "regions"=>  [region1, region2],
+  "essential_bcs"=>[e1, e2, e3], "traction_bcs"=>  [flux1])
 
-    # Write out mesh with stresses
-    dadd!(modeldata,postprocessing=dmake(file=  "multimaterial_beam", output=:Cauchy, component=:xy))
-    modeldata=JFinEALE.AlgoDeformationLinearModule.exportstress(modeldata)
+  # Call the solver
+  modeldata = AlgoDeforLinearModule.linearstatics(modeldata)
+  geom = modeldata["geom"]
+  u = modeldata["u"]
 
-    # Write out mesh with stresses
-    dadd!(modeldata,postprocessing=dmake(file=  "multimaterial_beam", output=:Cauchy, component=:xz))
-    modeldata=JFinEALE.AlgoDeformationLinearModule.exportstress(modeldata)
+  # Write out mesh with displacements
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam")
+  modeldata = AlgoDeforLinearModule.exportdeformation(modeldata)
 
-    # Write out mesh with von Mises stresses
-    dadd!(modeldata,postprocessing=dmake(file=  "multimaterial_beam", output=:vm))
-    modeldata=JFinEALE.AlgoDeformationLinearModule.exportstress(modeldata)
+  # Write out mesh with stresses
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_xy",
+    "quantity"=> :Cauchy, "component"=> :xy)
+  modeldata = AlgoDeforLinearModule.exportstress(modeldata)
 
-    # Write out mesh with von Mises stresses, elementwise
-    dadd!(modeldata,postprocessing=dmake(file=  "multimaterial_beam-ew", output=:vm))
-    modeldata=JFinEALE.AlgoDeformationLinearModule.exportstresselementwise(modeldata)
+  # Write out mesh with stresses
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_xz",
+    "quantity"=> :Cauchy, "component"=> :xz)
+  modeldata = AlgoDeforLinearModule.exportstress(modeldata)
 
-    # Write out mesh with von Mises stresses, elementwise
-    dadd!(modeldata,postprocessing=dmake(file=  "multimaterial_beam-ew", output=:Cauchy, component=:xz))
-    modeldata=JFinEALE.AlgoDeformationLinearModule.exportstresselementwise(modeldata)
+  # Write out mesh with von Mises stresses
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_vm",
+  "quantity"=> :vm)
+  modeldata = AlgoDeforLinearModule.exportstress(modeldata)
 
-    true
+  # Write out mesh with von Mises stresses, elementwise
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_vm-ew",
+  "quantity"=> :vm)
+  modeldata = AlgoDeforLinearModule.exportstresselementwise(modeldata)
+
+  true
 
 end
 multimaterial_beam()
-    
--- test/test_linear_deformation.jl
+++ test/test_linear_deformation.jl
@@ -1178,3 +1178,100 @@ end
 end
 using mmmmmmmmmZenkourm
 mmmmmmmmmZenkourm.test()
+
+module mmmmmmultimaterial_beam_xz
+using FinEtools
+using Base.Test
+function test()
+  # println("""
+  # Multi-material beam. Rubber-like and metal-like halfs,
+  # clamped, with shear traction at free end.
+  # """)
+  E1 = 0.29e3;
+  nu1 = 0.49;
+  E2 = 0.4e4;
+  nu2 = 0.3;
+  W = 4.1;
+  L = 12.;
+  t = 6.5;
+  nl = 2; nt = 1; nw = 1; ref = 9;
+  p =   200./W/t;
+  #  Loading in the Z direction
+  loadv = [0;0;p]; dir = 3;
+  tolerance  = t/1000;
+
+  fens,fes  = H20block(L,W,t, nl*ref,nw*ref,nt*ref)
+
+  # Clamped end of the beam
+  l1  = selectnode(fens; box = [0 0 -Inf Inf -Inf Inf], inflate  =  tolerance)
+  e1 = FDataDict("node_list"=>l1, "component"=>1, "displacement"=>0.0)
+  e2 = FDataDict("node_list"=>l1, "component"=>2, "displacement"=>0.0)
+  e3 = FDataDict("node_list"=>l1, "component"=>3, "displacement"=>0.0)
+
+  # Traction on the opposite edge
+  boundaryfes  =   meshboundary(fes);
+  Toplist   = selectelem(fens,boundaryfes, box =  [L L -Inf Inf -Inf Inf], inflate =   tolerance);
+  el1femm  =   FEMMBase(GeoD(subset(boundaryfes,Toplist), GaussRule(2, 2)))
+  flux1 = FDataDict("femm"=>el1femm, "traction_vector"=>loadv)
+
+  r1list   = selectelem(fens,fes, box =  [0 L/2. -Inf Inf -Inf Inf], inflate =   tolerance);
+  r2list   = selectelem(fens,fes, box =  [L/2. L -Inf Inf -Inf Inf], inflate =   tolerance);
+
+  # Model reduction type
+  MR = DeforModelRed3D
+
+  # Make region 1
+  region1 = FDataDict("femm"=>FEMMDeforLinear(MR,
+  GeoD(subset(fes,r1list), GaussRule(3,2)),
+  MatDeforElastIso(MR, 0.0, E1, nu1, 0.0)))
+
+  # Make region 2
+  region2 = FDataDict("femm"=>FEMMDeforLinear(MR,
+  GeoD(subset(fes,r2list), GaussRule(3,2)),
+  MatDeforElastIso(MR, 0.0, E2, nu2, 0.0)))
+
+  # Make model data
+  modeldata =  FDataDict(
+  "fens"=> fens, "regions"=>  [region1, region2],
+  "essential_bcs"=>[e1, e2, e3], "traction_bcs"=>  [flux1])
+
+  # Call the solver
+  modeldata = AlgoDeforLinearModule.linearstatics(modeldata)
+  geom = modeldata["geom"]
+  u = modeldata["u"]
+
+  # Write out mesh with displacements
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam")
+  modeldata = AlgoDeforLinearModule.exportdeformation(modeldata)
+  rm(modeldata["postprocessing"]["file"])
+
+  # Write out mesh with stresses
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_xy",
+  "quantity"=> :Cauchy, "component"=> :xy)
+  modeldata = AlgoDeforLinearModule.exportstress(modeldata)
+  rm(modeldata["postprocessing"]["file"])
+
+  # Write out mesh with stresses
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_xz",
+  "quantity"=> :Cauchy, "component"=> :xz)
+  modeldata = AlgoDeforLinearModule.exportstress(modeldata)
+  rm(modeldata["postprocessing"]["file"])
+
+  # Write out mesh with von Mises stresses
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_vm",
+  "quantity"=> :vm)
+  modeldata = AlgoDeforLinearModule.exportstress(modeldata)
+  rm(modeldata["postprocessing"]["file"])
+
+  # Write out mesh with von Mises stresses, elementwise
+  modeldata["postprocessing"] = FDataDict("file"=>"multimaterial_beam_vm-ew",
+  "quantity"=> :vm)
+  modeldata = AlgoDeforLinearModule.exportstresselementwise(modeldata)
+  rm(modeldata["postprocessing"]["file"])
+
+  true
+
+end
+end
+using mmmmmmultimaterial_beam_xz
+mmmmmmultimaterial_beam_xz.test()
