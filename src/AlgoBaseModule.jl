404b8b84dd4cf7a9422a1d5e56ec1df5e5c062b9
PetrKrysl <pkrysl@ucsd.edu>
2017-07-07 00:34:15 -0700

    Added Richardson extrapolation

--- src/AlgoBaseModule.jl
+++ src/AlgoBaseModule.jl
@@ -1,8 +1,12 @@
 module AlgoBaseModule
 
+using FinEtools.FTypesModule
+
+export FDataDict
+export dcheck!, richextrapol
+
 # Type for the model-data packaging system (used by all FinEtools algorithms)
 const FDataDict = Dict{String, Any}
-export FDataDict
 
 function dmatch(key::String, allowed_keys::Array{String})
   matched_key = nothing
@@ -29,6 +33,43 @@ function dcheck!(d::FDataDict, recognized_keys::Array{String})
     end
   end
 end
-export dcheck!
+
+"""
+    richextrapol(solns::AbstractArray{Real}, params::AbstractArray{Real})
+
+Richardson extrapolation.
+
+`solns` =  array of solution values
+`params` = array of values of parameters for which the solns have been obtained
+
+This function is applicable only to fixed ratio between the mesh sizes,
+  `params[1]/params[2) = params[2)/params[3)`.
+Output:
+solnestim= estimate of the asymptotic solution from the data points in the solns array
+beta= convergence rate
+c = constant in the estimate "error=c*h^beta"
+residual = residual after equations from which the above quantities were
+     solved (this is a measure of how accurately was the system solved).
+"""
+function richextrapol(solns::FFltVec, params::FFltVec)
+  if abs(params[1]/params[2] - params[2]/params[3]) > 1e-6
+    warn("Ratio between mesh sizes mmust be fixed: params[1]/params[2] - params[2]/params[3]")
+  end
+  nsolns = solns./solns[1];
+  solnestim = ((-(nsolns[1]*nsolns[3]-nsolns[2]^2)/(2*nsolns[2]-nsolns[1]-nsolns[3])))*solns[1];
+  if (solnestim-solns[1]) <= 0
+    beta = log((solnestim-solns[2])/(solnestim-solns[3]))/log(params[2]/params[3]);
+  else
+    beta = log((solnestim-solns[1])/(solnestim-solns[3]))/log(params[1]/params[3]);
+  end
+  # just to check things, calculate the residual
+  c = (solnestim-solns[1])/params[1]^beta;
+  residual = zeros(FFlt,3)
+  for I =1:3
+    residual[I] = (solnestim-solns[I])-c*params[I]^beta; # this should be close to zero
+  end
+  return solnestim, beta, c, residual
+end
+
 
 end
