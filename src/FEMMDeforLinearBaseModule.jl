424257c4b17bdd3c53d607f19fdfc288da672eb3
PetrKrysl <pkrysl@ucsd.edu>
2017-07-10 09:35:20 -0700

    Mass matrix  in  abstract

--- src/FEMMDeforLinearBaseModule.jl
+++ src/FEMMDeforLinearBaseModule.jl
@@ -25,6 +25,30 @@ using FinEtools.MatDeforModule
 
 abstract type FEMMDeforLinearAbstract <: FEMMAbstractBase end
 
+function buffers(self::FEMMDeforLinearAbstract, geom::NodalField, u::NodalField)
+  const ndn = ndofs(u); # number of degrees of freedom per node
+  const nne = nodesperelem(self.geod.fes); # number of nodes for element
+  const sdim = ndofs(geom);            # number of space dimensions
+  const mdim = manifdim(self.geod.fes); # manifold dimension of the element
+  const nstrs = nstsstn(self.mr);  # number of stresses
+  const elmatdim = ndn*nne;             # dimension of the element matrix
+  # Prepare buffers
+  elmat = zeros(FFlt, elmatdim, elmatdim);      # element matrix -- buffer
+  conn = zeros(FInt, nne, 1); # element nodes -- buffer
+  x = zeros(FFlt, nne, sdim); # array of node coordinates -- buffer
+  dofnums = zeros(FInt, 1, elmatdim); # degree of freedom array -- buffer
+  loc = zeros(FFlt, 1, sdim); # quadrature point location -- buffer
+  J = eye(FFlt, sdim, mdim); # Jacobian matrix -- buffer
+  csmatTJ = zeros(FFlt, mdim, mdim); # intermediate result -- buffer
+  gradN = zeros(FFlt, nne, mdim); # intermediate result -- buffer
+  D = zeros(FFlt, nstrs, nstrs); # material stiffness matrix -- buffer
+  B = zeros(FFlt, nstrs, elmatdim); # strain-displacement matrix -- buffer
+  DB = zeros(FFlt, nstrs, elmatdim); # strain-displacement matrix -- buffer
+  elvecfix = zeros(FFlt, elmatdim, 1); # vector of prescribed displ. -- buffer
+  elvec = zeros(FFlt, elmatdim); # element vector -- buffer
+  return conn, x, dofnums, loc, J, csmatTJ, gradN, D, B, DB, elmat, elvec, elvecfix
+end
+
 """
     stiffness(self::FEMMDeforLinear, assembler::A,
           geom::NodalField{FFlt},
@@ -58,22 +82,59 @@ function nzebcloadsstiffness(self::FEMMDeforLinearAbstract,  assembler::A,
 end
 
 function nzebcloadsstiffness(self::FEMMDeforLinearAbstract,
-              geom::NodalField{FFlt},
-              u::NodalField{T}) where {T<:Number}
-    assembler = SysvecAssembler()
-    return  nzebcloadsstiffness(self, assembler, geom, u);
+  geom::NodalField{FFlt},
+  u::NodalField{T}) where {T<:Number}
+  assembler = SysvecAssembler()
+  return  nzebcloadsstiffness(self, assembler, geom, u);
 end
 
 """
-    mass(self::FEMMDeforLinear,  assembler::A,
-      geom::NodalField{FFlt},  u::NodalField{T}) where {A<:SysmatAssemblerBase, T<:Number}
+    mass(self::FEMMDeforLinearAbstract,  assembler::A,
+      geom::NodalField{FFlt},
+      u::NodalField{T}) where {A<:SysmatAssemblerBase, T<:Number}
 
-Compute the mass matrix.
+Compute the consistent mass matrix
+
+This is a general routine for the abstract linear-deformation  FEMM.
 """
 function mass(self::FEMMDeforLinearAbstract,  assembler::A,
   geom::NodalField{FFlt},
   u::NodalField{T}) where {A<:SysmatAssemblerBase, T<:Number}
-  return spzeros(u.nfreedofs, u.nfreedofs);
+  geod = self.geod
+  npts,  Ns,  gradNparams,  w,  pc = integrationdata(geod);
+  conn, x, dofnums, loc, J, csmatTJ, gradN, D, B, DB, elmat =
+                buffers(self, geom, u)  # Prepare buffers
+  rho::FFlt = self.material.mass_density; # mass density
+  NexpTNexp = Array{FFltMat}(1, npts);# basis f. matrix -- buffer
+  ndn = ndofs(u)
+  for j = 1:npts # This quantity is the same for all quadrature points
+    Nexp = zeros(FFlt, ndn, size(elmat,1))
+    for l1 = 1:length(conn)
+      Nexp[1:ndn, (l1-1)*ndn+1:(l1)*ndn] = eye(ndn)*Ns[j][l1];
+    end
+    NexpTNexp[j] = Nexp'*Nexp;
+  end
+  startassembly!(assembler,  size(elmat,1),  size(elmat,2),  count(geod.fes),
+  u.nfreedofs,  u.nfreedofs);
+  for i = 1:count(geod.fes) # Loop over elements
+    getconn!(geod.fes, conn, i);
+    gathervalues_asmat!(geom, x, conn);# retrieve element coordinates
+    fill!(elmat,  0.0); # Initialize element matrix
+    for j = 1:npts # Loop over quadrature points
+      At_mul_B!(loc, Ns[j], x);# Quadrature point location
+      At_mul_B!(J, x, gradNparams[j]); # calculate the Jacobian matrix
+      Jac = Jacobianvolume(geod, J, loc, conn, Ns[j]);
+      thefactor::FFlt =(rho*Jac*w[j]);
+      @inbounds for nx = 1:size(elmat,1) # Do: Me = Me + (Nexp'*Nexp) * (rho * Jac * w(j));
+        @inbounds for mx = 1:size(elmat,2)
+          elmat[mx, nx] = elmat[mx, nx] + NexpTNexp[j][mx, nx]*thefactor
+        end
+      end
+    end # Loop over quadrature points
+    gatherdofnums!(u,  dofnums,  conn);# retrieve degrees of freedom
+    assemble!(assembler,  elmat,  dofnums,  dofnums);# assemble symmetric matrix
+  end # Loop over elements
+  return makematrix!(assembler);
 end
 
 function mass(self::FEMMDeforLinearAbstract,
--- src/FEMMDeforLinearMSModule.jl
+++ src/FEMMDeforLinearMSModule.jl
@@ -7,7 +7,7 @@ system vectors for linear deformation models:  mean-strain  formulation.
 module FEMMDeforLinearMSModule
 
 export FEMMDeforLinearMSH8, FEMMDeforLinearMST10
-export stiffness, nzebcloadsstiffness, mass, thermalstrainloads,
+export stiffness, nzebcloadsstiffness, thermalstrainloads,
        inspectintegpoints
 
 using FinEtools.FTypesModule
@@ -205,11 +205,11 @@ function stiffness(self::FEMMDeforLinearAbstractMS, assembler::A,
   return makematrix!(assembler);
 end
 
-function stiffness(self::FEMMDeforLinearAbstractMS,
-            geom::NodalField{FFlt},  u::NodalField{T}) where {T<:Number}
-  assembler = SysmatAssemblerSparseSymm();
-  return stiffness(self, assembler, geom, u);
-end
+# function stiffness(self::FEMMDeforLinearAbstractMS,
+#             geom::NodalField{FFlt},  u::NodalField{T}) where {T<:Number}
+#   assembler = SysmatAssemblerSparseSymm();
+#   return stiffness(self, assembler, geom, u);
+# end
 
 # """
 #     nzebcloadsstiffness(self::FEMMDeforLinear,  assembler::A,
@@ -259,58 +259,7 @@ end
 #     return  nzebcloadsstiffness(self, assembler, geom, u);
 # end
 #
-# """
-#     mass(self::FEMMDeforLinear,  assembler::A,
-#       geom::NodalField{FFlt},  u::NodalField{T}) where {A<:SysmatAssemblerBase, T<:Number}
-#
-# Compute the mass matrix.
-# """
-# function mass(self::FEMMDeforLinear,  assembler::A,
-#   geom::NodalField{FFlt},
-#   u::NodalField{T}) where {A<:SysmatAssemblerBase, T<:Number}
-#   geod = self.geod
-#   npts,  Ns,  gradNparams,  w,  pc = integrationdata(geod);
-#   conn, x, dofnums, loc, J, csmatTJ, gradN, D, B, DB, elmat =
-#                 buffers(self, geom, u)  # Prepare buffers
-#   rho::FFlt = self.material.mass_density; # mass density
-#   NexpTNexp = Array{FFltMat}(1, npts);# basis f. matrix -- buffer
-#   ndn = ndofs(u)
-#   for j = 1:npts # This quantity is the same for all quadrature points
-#     Nexp = zeros(FFlt, ndn, size(elmat,1))
-#     for l1 = 1:length(conn)
-#       Nexp[1:ndn, (l1-1)*ndn+1:(l1)*ndn] = eye(ndn)*Ns[j][l1];
-#     end
-#     NexpTNexp[j] = Nexp'*Nexp;
-#   end
-#   startassembly!(assembler,  size(elmat,1),  size(elmat,2),  count(geod.fes),
-#   u.nfreedofs,  u.nfreedofs);
-#   for i = 1:count(geod.fes) # Loop over elements
-#     getconn!(geod.fes, conn, i);
-#     gathervalues_asmat!(geom, x, conn);# retrieve element coordinates
-#     fill!(elmat,  0.0); # Initialize element matrix
-#     for j = 1:npts # Loop over quadrature points
-#       At_mul_B!(loc, Ns[j], x);# Quadrature point location
-#       At_mul_B!(J, x, gradNparams[j]); # calculate the Jacobian matrix
-#       Jac = Jacobianvolume(geod, J, loc, conn, Ns[j]);
-#       thefactor::FFlt =(rho*Jac*w[j]);
-#       @inbounds for nx = 1:size(elmat,1) # Do: Me = Me + (Nexp'*Nexp) * (rho * Jac * w(j));
-#         @inbounds for mx = 1:size(elmat,2)
-#           elmat[mx, nx] = elmat[mx, nx] + NexpTNexp[j][mx, nx]*thefactor
-#         end
-#       end
-#     end # Loop over quadrature points
-#     gatherdofnums!(u,  dofnums,  conn);# retrieve degrees of freedom
-#     assemble!(assembler,  elmat,  dofnums,  dofnums);# assemble symmetric matrix
-#   end # Loop over elements
-#   return makematrix!(assembler);
-# end
-#
-# function mass(self::FEMMDeforLinear,
-#               geom::NodalField{FFlt},  u::NodalField{T}) where {T<:Number}
-#     assembler = SysmatAssemblerSparseSymm();
-#     return mass(self, assembler, geom, u);
-# end
-#
+
 # """
 #     thermalstrainloads(self::FEMMDeforLinear, assembler::A,
 #         geom::NodalField{FFlt}, u::NodalField{T},
