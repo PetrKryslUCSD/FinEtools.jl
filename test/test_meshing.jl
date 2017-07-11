659af2b64d7d9c992a8cf0999f83cd49d2f1ddf7
PetrKrysl <pkrysl@ucsd.edu>
2017-07-11 09:16:44 -0700

    Merge-mesh test added

--- test/test_meshing.jl
+++ test/test_meshing.jl
@@ -59,3 +59,40 @@ end
 end
 using mmmmmiimportexportmmmmm
 mmmmmiimportexportmmmmm.test()
+
+module mmmLshapemmmmmmm
+using FinEtools
+using Base.Test
+function test()
+  # println("""
+  # Meshing  for an L-shaped membrane.
+  # """)
+
+  t0 = time()
+
+  W= 100. # strip width
+  L = 200. # length of the strip
+  nL = 5
+  nW = 5
+  tolerance = W/nW/1.0e5
+  Meshes = Array{Tuple{FENodeSet, FESet},1}()
+  push!(Meshes, Q4quadrilateral([0.0 0.0; W W], nW, nW))
+  push!(Meshes, Q4quadrilateral([-L 0.0; 0.0 W], nL, nW))
+  push!(Meshes, Q4quadrilateral([0.0 -L; W 0.0], nW, nL))
+  fens, outputfes = mergenmeshes(Meshes, tolerance);
+  fes = cat(outputfes[1], cat(outputfes[2], outputfes[3]))
+  @test count(fens) == 96
+  @test norm(vec(fes.conn[1,:]) - vec([1   2   8   7])) < 0.01
+  geom = NodalField(fens.xyz)
+
+  File =  "L_shape.vtk"
+  vtkexportmesh(File, fes.conn, geom.values, FinEtools.MeshExportModule.Q4);
+  # @async run(`"paraview.exe" $File`)
+  rm(File)
+
+  # println("Done")
+  true
+end
+end
+using mmmLshapemmmmmmm
+mmmLshapemmmmmmm.test()
