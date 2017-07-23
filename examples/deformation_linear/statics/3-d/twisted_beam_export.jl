067b111760db6cf1b79746b26b696f1a718c3e0c
PetrKrysl <pkrysl@ucsd.edu>
2017-07-23 11:27:12 -0700

    Abaqus export example

--- examples/deformation_linear/statics/3-d/twisted_beam_export.jl
+++ examples/deformation_linear/statics/3-d/twisted_beam_export.jl
@@ -1,22 +1,12 @@
 using FinEtools
 using FinEtools.AlgoDeforLinearModule
-using AbaqusExportModule
+using FinEtools.AbaqusExportModule
 
 println("""
-The initially twisted cantilever beam is one of the standard test
-problems for verifying the finite-element accuracy [1]. The beam is
-clamped at one end and loaded either with unit in-plane or
-unit out-of-plane force at the other. The centroidal axis of the beam is
-straight at the undeformed  configuration, while its cross-sections are
-twisted about the centroidal axis from 0 at the clamped end to pi/2 at
-the free end.
+Refer to twisted_beam.jl.
 
-This example EXPORTS the model to Abaqus.
-
-Reference:
-Zupan D, Saje M (2004) On "A proposed standard set of problems to test
-finite element accuracy": the twisted beam. Finite Elements in Analysis
-and Design 40: 1445-1451.
+This example EXPORTS the model to Abaqus. Import the .inp file
+into Abaqus and run the job.
 """)
 function  Twisted_beam_export()
   E = 0.29e8;
@@ -24,7 +14,7 @@ function  Twisted_beam_export()
   W = 1.1;
   L = 12.;
   t =  0.32;
-  nl = 2; nt = 1; nw = 1; ref = 7;
+  nl = 2; nt = 1; nw = 1; ref = 3;
   p =   1/W/t;
   #  Loading in the Z direction
   loadv = [0;0;p]; dir = 3; uex = 0.005424534868469; # Harder: 5.424e-3;
@@ -64,16 +54,32 @@ function  Twisted_beam_export()
   "fens"=> fens, "regions"=>  [region1],
   "essential_bcs"=>[e1, e2, e3], "traction_bcs"=>  [flux1])
 
+
   AE = AbaqusExporter("twisted_beam");
   HEADING(AE, "Twisted beam example");
   PART(AE, "part1");
-  NODE(AE, fens.xyz);
-  ELEMENT(AE, "c3d8", "Allh8", 1, fes.conn)
   END_PART(AE);
   ASSEMBLY(AE, "ASSEM1");
-  INSTANCE(AE, "INSTNC1","PART1");
+  INSTANCE(AE, "INSTNC1", "PART1");
+  NODE(AE, fens.xyz);
+  ELEMENT(AE, "c3d8rh", "AllElements", 1, region1["femm"].geod.fes.conn)
+  ELEMENT(AE, "SFM3D4", "TractionElements",
+    1+count(region1["femm"].geod.fes), flux1["femm"].geod.fes.conn)
+  NSET_NSET(AE, "l1", l1)
+  ORIENTATION(AE, "GlobalOrientation", vec([1. 0 0]), vec([0 1. 0]));
+  SOLID_SECTION(AE, "elasticity", "GlobalOrientation", "AllElements", "Hourglass");
+  SURFACE_SECTION(AE, "TractionElements")
   END_INSTANCE(AE);
   END_ASSEMBLY(AE);
+  MATERIAL(AE, "elasticity")
+  ELASTIC(AE, E, nu)
+  SECTION_CONTROLS(AE, "section1", "HOURGLASS=ENHANCED")
+  STEP_PERTURBATION_STATIC(AE)
+  BOUNDARY(AE, "ASSEM1.INSTNC1.l1", 1)
+  BOUNDARY(AE, "ASSEM1.INSTNC1.l1", 2)
+  BOUNDARY(AE, "ASSEM1.INSTNC1.l1", 3)
+  DLOAD(AE, "ASSEM1.INSTNC1.TractionElements", vec(flux1["traction_vector"]))
+  END_STEP(AE)
   close(AE)
 
   true
