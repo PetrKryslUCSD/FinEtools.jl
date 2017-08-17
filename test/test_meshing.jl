c2703e0f3862a180370f556041617df1ede2f7dd
Petr <pkrysl@ucsd.edu>
2017-08-16 19:39:01 -0700

    Couple more tests

--- test/test_meshing.jl
+++ test/test_meshing.jl
@@ -414,3 +414,53 @@ end
 end
 using mmmMergem2
 mmmMergem2.test()
+
+
+module mmmMergem3
+using FinEtools
+using Base.Test
+function test()
+    h = 0.05*phun("M");
+    l = 10*h;
+    Rmed = h/0.2;
+    psi   = 0;    # Cylinder
+    nh = 12; nl  = 40; nc = 120;
+    nh = 6; nl  = 20; nc = 60;
+    # nh = 3; nl  = 8; nc = 30;
+    tolerance = h/nh/100;
+
+    t0 = time()
+    MR = DeforModelRed3D
+    fens,fes  = H8block(h,l,2.0*pi,nh,nl,nc)
+    # Shape into a cylinder
+    R = zeros(3, 3)
+    for i = 1:count(fens)
+        x, y, z = fens.xyz[i,:];
+        rotmat3!(R, [0, z, 0])
+        Q = [cos(psi*pi/180) sin(psi*pi/180) 0;
+            -sin(psi*pi/180) cos(psi*pi/180) 0;
+            0 0 1]
+        fens.xyz[i,:] = reshape([x+Rmed-h/2, y-l/2, 0], 1, 3)*Q*R;
+    end
+    # println("  before merging  = $(count(fens))")
+    @test count(fens) == 8967
+    # File =  "unit_cube_modes.vtk"
+    # vtkexportmesh(File, fens, fes)
+    # @async run(`"paraview.exe" $File`)
+
+    candidates = selectnode(fens, plane = [0.0 0.0 1.0 0.0], thickness = h/1000)
+    fens,fes = mergenodes(fens, fes,  tolerance, candidates);
+    @test count(fens) == 8820
+
+    # fens,fes = mergenodes(fens, fes,  tolerance);
+    # println("  after merging  = $(count(fens))")
+    #
+    # println("Mesh generation ($(time() - t0) sec)")
+    #
+    # File =  "unit_cube_modes.vtk"
+    # vtkexportmesh(File, fens, fes)
+    # @async run(`"paraview.exe" $File`)
+end
+end
+using mmmMergem3
+mmmMergem3.test()
