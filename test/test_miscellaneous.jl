efda11926afa042b5af508d0f742a01661ebaeea
Petr <pkrysl@ucsd.edu>
2017-08-18 09:46:23 -0700

    three-space test

--- test/test_miscellaneous.jl
+++ test/test_miscellaneous.jl
@@ -821,3 +821,54 @@ end
 end
 using mxmeasurementm4
 mxmeasurementm4.test()
+
+module mmmiscellaneous2
+using FinEtools
+using FinEtools.MeshExportModule
+using Base.Test
+function test()
+  rho=1.21*1e-9;# mass density
+  c =345.0*1000;# millimeters per second
+  bulk= c^2*rho;
+  Lx=1900.0;# length of the box, millimeters
+  Ly=800.0; # length of the box, millimeters
+
+  fens,fes = Q4block(Lx,Ly,3,2); # Mesh
+  X = xyz3(fens)
+  fens.xyz = deepcopy(X)
+  # show(fes.conn)
+  # File = "mesh.vtk"
+  # MeshExportModule.vtkexportmesh(File, fens, fes)
+
+  bfes = meshboundary(fes)
+  @test bfes.conn == [1 2; 5 1; 2 3; 3 4; 4 8; 9 5; 8 12; 10 9; 11 10; 12 11]
+end
+end
+using mmmiscellaneous2
+mmmiscellaneous2.test()
+
+module mmmiscellaneous3
+using FinEtools
+using FinEtools.MeshExportModule
+using Base.Test
+function test()
+  rho=1.21*1e-9;# mass density
+  c =345.0*1000;# millimeters per second
+  bulk= c^2*rho;
+  Lx=1900.0;# length of the box, millimeters
+  Ly=800.0; # length of the box, millimeters
+
+  fens,fes = L2block(Lx,4); # Mesh
+  X = xyz3(fens)
+  fens.xyz = deepcopy(X)
+  # show(fes.conn)
+  # File = "mesh.vtk"
+  # MeshExportModule.vtkexportmesh(File, fens, fes)
+
+  bfes = meshboundary(fes)
+  # show(fes.conn)
+  @test vec(bfes.conn) == vec([1 5])
+end
+end
+using mmmiscellaneous3
+mmmiscellaneous3.test()
