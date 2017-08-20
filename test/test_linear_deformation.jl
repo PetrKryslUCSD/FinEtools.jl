49efaba66f8e7e329ffa4fe62a07e799164caa74
Petr <pkrysl@ucsd.edu>
2017-08-20 13:43:18 -0700

    Test of anisotropic--material deformation

--- test/test_linear_deformation.jl
+++ test/test_linear_deformation.jl
@@ -5243,7 +5243,7 @@ function test()
         maxiter = 20
         lamb, v, nconv, niter, nmult, lamberr =
             solver(K+OmegaShift*M, M; nev=neigvs, v0=v0, tol=tol, maxiter=maxiter, withrr=true)
-        @test nconv != neigvs
+        @test nconv == neigvs
         # if nconv < neigvs
             # println("NOT converged")
         # end
@@ -5264,3 +5264,162 @@ end
 end
 using mmtruncatedmfreem2
 mmtruncatedmfreem2.test()
+
+module mfiber_reinf_cant_yn_strong_Abaqus
+using FinEtools
+using FinEtools.AlgoDeforLinearModule
+using Base.Test
+function test()
+
+
+# println("""
+# Cantilever example.  Strongly orthotropic material. Orientation "y".
+# @article{
+# author = {Krysl, P.},
+# title = {Mean-strain 8-node hexahedron with optimized energy-sampling stabilization},
+# journal = {Finite Elements in Analysis and Design},
+# volume = {108}, pages = {41-53}, DOI = {10.1016/j.finel.2015.09.008}, year = {2016}
+# }
+# """)
+
+t0 = time()
+pu = ustring -> phun(ustring; system_of_units = :SIMM)
+
+# # Orthotropic material
+E1s = 100000.0*pu("GPa")
+E2s = 1.0*pu("GPa")
+E3s = E2s
+nu23s = nu12s = nu13s = 0.25
+G12s = 0.2*pu("GPa")
+G23s = G13s = G12s
+CTE1 = 0.0
+CTE2 = 0.0
+CTE3 = 0.0
+# # Isotropic material
+# E = 1.0e9*pu("Pa")
+# nu = 0.25
+# CTE = 0.0
+
+# Reference value for  the vertical deflection of the tip
+uz_ref = -1.027498445054843e-05*pu("m");
+
+a = 90.0*pu("mm") # length of the cantilever
+b = 10.0*pu("mm") # width of the cross-section
+t = 20.0*pu("mm") # height of the cross-section
+q0 = -1000.0*pu("Pa") # shear traction
+dT = 0*pu("K") # temperature rise
+
+tolerance = 0.00001*t
+
+# Generate mesh
+n = 2
+na = 4*n # number of elements lengthwise
+nb = 2*n # number of elements through the depth
+nt = n # number of elements through the thickness
+xs = collect(linspace(0.0, a, na+1))
+ys = collect(linspace(0.0, b, nb+1))
+ts = collect(linspace(0.0, t, nt+1))
+# println("Mesh generation")
+fens,fes = H8blockx(xs, ys, ts)
+fens,fes = H8toH20(fens,fes)
+
+bfes = meshboundary(fes)
+# end cross-section surface  for the shear loading
+sshearl = selectelem(fens, bfes; facing=true, direction = [+1.0 0.0 0.0])
+
+MR = DeforModelRed3D
+material = MatDeforElastOrtho(MR,
+  0.0, E1s, E2s, E3s,
+  nu12s, nu13s, nu23s,
+  G12s, G13s, G23s,
+  CTE1, CTE2, CTE3)
+# material = MatDeforElastIso(MR,
+#   0.0, E, nu, CTE)
+
+# Material orientation matrix
+csmat = zeros(3, 3)
+rotmat3!(csmat, -45.0/180.0*pi*[0,1,0])
+
+function updatecs!(csmatout::FFltMat, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+  copy!(csmatout, csmat)
+end
+
+femm = FEMMDeforLinear(MR, GeoD(fes, GaussRule(3, 2), CSys(3, 3, updatecs!)), material)
+
+lx0 = selectnode(fens, box=[0.0 0.0 -Inf Inf -Inf Inf], inflate=tolerance)
+
+geom = NodalField(fens.xyz)
+u = NodalField(zeros(size(fens.xyz,1),3)) # displacement field
+nnodes(geom)
+
+setebc!(u, lx0, true, 1, zeros(size(lx0)))
+setebc!(u, lx0, true, 2, zeros(size(lx0)))
+setebc!(u, lx0, true, 3, zeros(size(lx0)))
+applyebc!(u)
+
+# S = connectionmatrix(femm.femmbase, nnodes(geom))
+numberdofs!(u)
+
+function getshr!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+  copy!(forceout, q0*[0.0; 0.0; 1.0])
+end
+
+Tracfemm = FEMMBase(GeoD(subset(bfes, sshearl), GaussRule(2, 3)))
+
+# println("Stiffness")
+K = stiffness(femm, geom, u)
+fi = ForceIntensity(FFlt, 3, getshr!);
+# println("Traction loads")
+F =  distribloads(Tracfemm, geom, u, fi, 2);
+
+# println("Factorization")
+K = (K + K')/2;
+K = cholfact(Symmetric(K))
+# println("U = K\\F")
+U = K\F
+# # println("U = cg(K, F; tol=1e-3, maxiter=2000)")
+# U = cg(K, F; tol=1e-3, maxiter=2000)
+scattersysvec!(u, U[:])
+
+Tipl = selectnode(fens, box=[a a b b 0. 0.], inflate=tolerance)
+utip = mean(u.values[Tipl, 3])
+# println("Deflection $utip, normalized: $(utip/uz_ref)")
+@test abs(utip - -0.00653888266072445)/abs(utip) < 1.0e-6
+# println("Solution: $(  time()-t0 )")
+
+AE = AbaqusExporter("fiber_reinf_cant_yn_strong_Abaqus");
+HEADING(AE, "fiber_reinf_cant_yn_strong_Abaqus.");
+PART(AE, "part1");
+END_PART(AE);
+ASSEMBLY(AE, "ASSEM1");
+INSTANCE(AE, "INSTNC1", "PART1");
+NODE(AE, fens.xyz);
+ELEMENT(AE, "c3d20r", "AllElements", femm.geod.fes.conn)
+ELEMENT(AE, "SFM3D8", "TractionElements", Tracfemm.geod.fes.conn)
+NSET_NSET(AE, "L1", lx0)
+NSET_NSET(AE, "L2", lx0)
+NSET_NSET(AE, "L3", lx0)
+NSET_NSET(AE, "tip", Tipl)
+ORIENTATION(AE, "GlobalOrientation", vec([1. 0 0]), vec([0 1. 0]));
+ORIENTATION(AE, "MaterialOrientation", vec(csmat[:,1]), vec(csmat[:,2]));
+SOLID_SECTION(AE, "elasticity", "MaterialOrientation", "AllElements", "Hourglassctl");
+SURFACE_SECTION(AE, "TractionElements")
+END_INSTANCE(AE);
+END_ASSEMBLY(AE);
+MATERIAL(AE, "elasticity")
+ELASTIC(AE, E1s, E2s, E3s, nu12s, nu13s, nu23s, G12s, G13s, G23s)
+SECTION_CONTROLS(AE, "Hourglassctl", "HOURGLASS=ENHANCED")
+STEP_PERTURBATION_STATIC(AE)
+BOUNDARY(AE, "ASSEM1.INSTNC1.L1", 1)
+BOUNDARY(AE, "ASSEM1.INSTNC1.L2", 2)
+BOUNDARY(AE, "ASSEM1.INSTNC1.L3", 3)
+DLOAD(AE, "ASSEM1.INSTNC1.TractionElements", vec([0.0, 0.0, q0]))
+END_STEP(AE)
+close(AE)
+
+# println("Done: $(  time()-t0 )")
+true
+end
+end
+using mfiber_reinf_cant_yn_strong_Abaqus
+mfiber_reinf_cant_yn_strong_Abaqus.test()
