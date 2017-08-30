d68735f3a9f369cac3fc0f43fd9cde1fe00c6863
Petr <pkrysl@ucsd.edu>
2017-08-30 09:31:32 -0700

    update ex

--- examples/deformation_linear/statics/2-d/LE1NAFEMS_Q8_stress.jl
+++ examples/deformation_linear/statics/2-d/LE1NAFEMS_Q8_stress.jl
@@ -1,6 +1,6 @@
-using JFinEALE
+using FinEtools
 
-println("LE1NAFEMS, plane stress, Q8 elements."        )        
+println("LE1NAFEMS, plane stress.")
 t0 = time()
 
 E = 210e3*phun("MEGA*PA");# 210e3 MPa
@@ -8,11 +8,10 @@ nu = 0.3;
 p = 10*phun("MEGA*PA");# 10 MPA Outward pressure on the outside ellipse
 sigma_yD= 92.7*phun("MEGA*PA");# tensile stress at [2.0, 0.0] meters
 Radius= 1.0*phun("m")
-n=3; # number of elements per side
+n=20; # number of elements per side
 tolerance=1.0/n/1000.;#Geometrical tolerance
 
-fens,fes =Q8block(1.0,pi/2, n, n*2)
-setotherdimension!(fes,1.0)
+fens,fes = Q8block(1.0,pi/2, n, n*2)
 
 bdryfes = meshboundary(fes);
 icl = selectelem(fens, bdryfes, box=[1.0,1.0,0.0,pi/2],inflate=tolerance);
@@ -22,58 +21,50 @@ for i=1:count(fens)
 end
 
 
-geom = NodalField(name ="geom",data =fens.xyz)
-u = NodalField(name ="u",data =zeros(size(fens.xyz,1),2)) # displacement field
+geom = NodalField(fens.xyz)
+u = NodalField(zeros(size(fens.xyz,1),2)) # displacement field
 
 l1 =selectnode(fens; box=[0.0 Inf 0.0 0.0], inflate = tolerance)
-setebc!(u,l1,trues(length(l1)),l1*0+2,[0.0])
+setebc!(u,l1,true,2,0.0)
 l1 =selectnode(fens; box=[0.0 0.0 0.0 Inf], inflate = tolerance)
-setebc!(u,l1,trues(length(l1)),l1*0+1,[0.0])    
+setebc!(u,l1,true,1,0.0)
 
 applyebc!(u)
 numberdofs!(u)
 
-el1femm =  FEMMBase(subset(bdryfes,icl), GaussRule(order=2,dim=1))
-function pfun(x::JFFltMat,J::JFFltMat,l::JFInt)
-    pt= [2.75/3.25*x[1],3.25/2.75*x[2]]
-    return   (p*pt'/norm(pt));
+el1femm =  FEMMBase(GeoD(subset(bdryfes,icl), GaussRule(1, 3)))
+function pfun(forceout::FFltVec, x::FFltMat, J::FFltMat, l::FInt)
+    pt= [2.75/3.25*x[1] 3.25/2.75*x[2]]
+    copy!(forceout, vec(p*pt/norm(pt)))
+    return forceout
 end
-fi = ForceIntensity(JFFlt,pfun);
+fi = ForceIntensity(FFlt, 2, pfun);
 F2= distribloads(el1femm, geom, u, fi, 2);
 
 
-p=PropertyDeformationLinearIso(E,nu)
-material=MaterialDeformationLinear (p)
+material=MatDeforElastIso(MR, E, nu)
+MR = DeforModelRed2DStress
+femm = FEMMDeforLinear(MR, GeoD(fes, GaussRule(2, 3)), material)
 
-femm = FEMMDeformationLinear(FEMMBase(fes, GaussRule(order=2,dim=2)), material)
-
-K =stiffness(DeformationModelReduction2DStress, femm, geom, u)
+K =stiffness(femm, geom, u)
 K=cholfact(K)
 U=  K\(F2)
 scattersysvec!(u,U[:])
 
-nl=selectnode (fens, box=[2.0,2.0,0.0,0.0],inflate=tolerance);
-thecorneru=zeros(JFFlt,1,2)
-gathervaluesasmat!(u,thecorneru,nl);
+nl=selectnode(fens, box=[2.0,2.0,0.0,0.0],inflate=tolerance);
+thecorneru=zeros(FFlt,1,2)
+gathervalues_asmat!(u,thecorneru,nl);
 thecorneru=thecorneru/phun("mm")
 println("$(time()-t0) [s];  displacement =$(thecorneru) [MM] as compared to reference [-0.10215,0] [MM]")
 
-function inspector(idat,out,loc,pc)
-    println("$(  loc  ): $(  out  )")
-    return idat
-end
-felist=[1]
-inspectintegpoints(DeformationModelReduction2DStress,femm, 
-                   geom, u,
-                   felist,
-                   inspector, [])
+fld= fieldfromintegpoints(femm, geom, u, :Cauchy, 2)
+println("  Target stress: $(fld.values[nl][1]/phun("MEGA*PA")) compared to $(sigma_yD/phun("MEGA*PA"))")
 
-fld= fieldfromintegpoints(DeformationModelReduction2DStress,femm, 
-                          geom, u, :Cauchy, 2)
-                          
-                          using JFinEALE.MeshExportModule
+using FinEtools.MeshExportModule
 
 File =  "a.vtk"
-vtkexportmesh(File, fens, fes; scalars=fld.values,scalars_name ="sigmay")
-
+vtkexportmesh(File, fes.conn, geom.values,
+               FinEtools.MeshExportModule.Q8;
+               vectors=[("u", u.values)], scalars=[("sigy", fld.values)])
+@async run(`"paraview.exe" $File`)
 true
