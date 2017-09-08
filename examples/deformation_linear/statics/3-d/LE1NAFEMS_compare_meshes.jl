4b46de056b32b553d500700562ccc8aa19380791
Petr <pkrysl@ucsd.edu>
2017-09-08 14:08:58 -0700

    Plate  with hole eexamples

---
+++ examples/deformation_linear/statics/3-d/LE1NAFEMS_compare_meshes.jl
@@ -0,0 +1,57 @@
+using FinEtools
+using FinEtools.MeshExportModule
+using FinEtools.MeshImportModule: import_ABAQUS
+
+Thick0 = 0.1*phun("m")/2.0 # to account for the symmetry reduction
+
+ref  = 0
+Thickness = Thick0
+tolerance = Thickness/2^ref/300.; # Geometrical tolerance
+
+
+fens,fes = T10block(1.0, pi/2, Thickness, 2^ref*5, 2^ref*6, 1; orientation = :b)
+for i=1:count(fens)
+    t=fens.xyz[i,1]; a=fens.xyz[i,2]; z=fens.xyz[i,3]
+    fens.xyz[i,:]=[(t*3.25+(1-t)*2)*cos(a), (t*2.75+(1-t)*1)*sin(a), z];
+end
+println("$((count(fens), count(fes)))")
+
+fens1,fes1 = import_ABAQUS("LE1AbaqusExport-C3D10HS-5-6-1.inp")
+println("$((count(fens1), count(fes1[1])))")
+
+ fens, newfes1, fes2 = mergemeshes(fens,fes, fens1,fes1[1], tolerance)
+ # fes = cat(fes2, newfes1)
+ # println("$((count(fens), count(fes)))")
+
+File =  "a1.vtk"
+vtkexportmesh(File, newfes1.conn, fens.xyz,
+               FinEtools.MeshExportModule.T10)
+@async run(`"paraview.exe" $File`)
+File =  "a2.vtk"
+vtkexportmesh(File, fes2.conn, fens.xyz,
+               FinEtools.MeshExportModule.T10)
+@async run(`"paraview.exe" $File`)
+
+
+#
+# fens,fes = H8block(1.0, pi/2, Thickness, 2^ref*5, 2^ref*6, 1)
+# for i=1:count(fens)
+#     t=fens.xyz[i,1]; a=fens.xyz[i,2]; z=fens.xyz[i,3]
+#     fens.xyz[i,:]=[(t*3.25+(1-t)*2)*cos(a), (t*2.75+(1-t)*1)*sin(a), z];
+# end
+# println("$((count(fens), count(fes)))")
+#
+# fens1,fes1 = import_ABAQUS("LE1AbaqusExport-C3D8S-80-96-1.inp")
+# println("$((count(fens1), count(fes1[1])))")
+#
+#  fens, newfes1, fes2 = mergemeshes(fens,fes, fens1,fes1[1], tolerance)
+#  fes = cat(fes2, newfes1)
+#  println("$((count(fens), count(fes)))")
+#
+# File =  "a.vtk"
+# vtkexportmesh(File, fes.conn, fens.xyz,
+#                FinEtools.MeshExportModule.H8)
+# @async run(`"paraview.exe" $File`)
+
+
+true
---
+++ examples/deformation_linear/statics/3-d/plate_w_hole_MSH8_convergence.jl
@@ -0,0 +1,164 @@
+module mplate_w_hole_MSH8m
+using FinEtools
+using FinEtools.MeshExportModule
+using DataFrames
+using CSV
+using Base.Test
+function test()
+    E = 2.4*phun("MEGA*PA");# 210e3 MPa
+    nu = 0.49995;
+    Re = 0.3*phun("M"); # outer radius
+    Ri= 0.1*phun("M"); # hole radius
+    H = 0.1*phun("M") # thickness of the plate
+    nRadial, nCircumferential=3, 5;
+    sigma0=1*phun("MEGA*PA");
+
+    function sigmaxx(x)
+        local r = norm(x[1:2]);
+        local th = atan2(x[2],x[1]);
+        return sigma0*(1-Ri^2/r^2*(3/2*cos(2*th)+cos(4*th))+3/2*Ri^4/r^4*cos(4*th));
+    end
+    function sigmayy(x)
+        local r=norm(x[1:2]);
+        local th = atan2(x[2],x[1]);
+        return -sigma0*(Ri^2/r^2*(1/2*cos(2*th)-cos(4*th))+3/2*Ri^4/r^4*cos(4*th));
+    end
+    function sigmaxy(x)
+        local r=norm(x[1:2]);
+        local th = atan2(x[2],x[1]);
+        return -sigma0*(Ri^2/r^2*(1/2*sin(2*th)+sin(4*th))-3/2*Ri^4/r^4*sin(4*th));
+    end
+    function sigmarr(x)
+        local r = norm(x[1:2]);
+        local th = atan2(x[2],x[1]);
+        return sigma0/2*(1-Ri^2/r^2) + sigma0/2*(1-4*Ri^2/r^2+3*Ri^4/r^4)*cos(2*th)
+    end
+    function sigmatt(x)
+        local r = norm(x[1:2]);
+        local th = atan2(x[2],x[1]);
+        return sigma0/2*(1+Ri^2/r^2) - sigma0/2*(1+3*Ri^4/r^4)*cos(2*th)
+    end
+    function sigmart(x)
+        local r = norm(x[1:2]);
+        local th = atan2(x[2],x[1]);
+        return -sigma0/2*(1+2*Ri^2/r^2-3*Ri^4/r^4)*sin(2*th)
+    end
+
+    sigxderrs = Dict{Symbol, FFltVec}()
+    sigyderrs = Dict{Symbol, FFltVec}()
+
+    nelems = []
+    for extrapolation in [:estimtrendpaper :estimtrend :default]
+        sigxderrs[extrapolation] = FFltVec[]
+        sigyderrs[extrapolation] = FFltVec[]
+        nelems = []
+        for ref in 0:1:5
+            # Thickness = H
+            Thickness = H/2^ref
+            tolerance = Thickness/2^ref/1000.; # Geometrical tolerance
+
+            fens,fes = H8block(1.0, pi/2, Thickness, 2^ref*nRadial, 2^ref*nCircumferential, 1)
+
+            bdryfes = meshboundary(fes);
+            icl = selectelem(fens, bdryfes, box=[1.0, 1.0, 0.0, pi/2, 0.0, Thickness], inflate=tolerance);
+
+            for i=1:count(fens)
+                t=fens.xyz[i,1]; a=fens.xyz[i,2]; z=fens.xyz[i,3]
+                fens.xyz[i,:] = [(t*Re+(1-t)*Ri)*cos(a), (t*Re+(1-t)*Ri)*sin(a), z];
+            end
+
+            # File =  "a.vtk"
+            # vtkexportmesh(File, fes.conn, fens.xyz,
+            #     FinEtools.MeshExportModule.H8)
+            # @async run(`"paraview.exe" $File`)
+
+            geom = NodalField(fens.xyz)
+            u = NodalField(zeros(size(fens.xyz,1),3)) # displacement field
+
+            l1 =selectnode(fens; box=[0.0, Inf, 0.0, 0.0, 0.0, Thickness], inflate = tolerance)
+            setebc!(u,l1,true, 2, 0.0)
+            l1 =selectnode(fens; box=[0.0, 0.0, 0.0, Inf, 0.0, Thickness], inflate = tolerance)
+            setebc!(u,l1,true, 1, 0.0)
+            # Plane-stress constraint: assume the plane z=0 is the plane of symmetry of the plate
+            l1 =selectnode(fens; box=[0.0, Inf, 0.0, Inf, 0.0, 0.0], inflate = tolerance)
+            setebc!(u,l1,true, 3, 0.0)
+            # If this was enabled, the plane-strain  constraint would be enforced.
+            # l1 =selectnode(fens; box=[0.0, Inf, 0.0, Inf, Thickness, Thickness], inflate = tolerance)
+            # setebc!(u,l1,true, 3, 0.0)
+
+            applyebc!(u)
+            numberdofs!(u)
+            el1femm =  FEMMBase(GeoD(subset(bdryfes,icl), GaussRule(2, 2)))
+            function pfun(forceout::FVec{T}, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt) where {T}
+                local r = sqrt(XYZ[1]^2 + XYZ[2]^2)
+                nx = XYZ[1]/r; ny = XYZ[2]/r
+                # local sx, sy, txy
+                # sx, sy, txy = sigmaxx(XYZ), sigmayy(XYZ), sigmaxy(XYZ)
+                # sn = sx * nx^2 + sy * ny^2 + 2 * nx * ny * txy
+                # tn = -(sx - sy) * nx * ny + (nx^2 - ny^2) * txy
+                # forceout[1] = sn * nx - tn * ny
+                # forceout[2] = sn * ny + tn * nx
+                # forceout[3] = 0.0
+                forceout[1] = sigmarr(XYZ) * nx - sigmart(XYZ) * ny
+                forceout[2] = sigmarr(XYZ) * ny + sigmart(XYZ) * nx
+                forceout[3] = 0.0
+                return forceout
+            end
+            fi = ForceIntensity(FFlt, 3, pfun);
+            F2 = distribloads(el1femm, geom, u, fi, 2);
+
+            MR = DeforModelRed3D
+
+            material = MatDeforElastIso(MR, E, nu)
+
+            femm = FEMMDeforLinearMSH8(MR, GeoD(fes, GaussRule(3, 2)), material)
+
+            # The geometry field now needs to be associated with the FEMM
+            femm = associategeometry!(femm, geom)
+
+            K = stiffness(femm, geom, u)
+            K = cholfact(K)
+            U = K\(F2)
+            scattersysvec!(u, U[:])
+
+            nlA = selectnode(fens, box=[Ri, Ri, 0.0, 0.0, 0.0, Thickness], inflate=tolerance);
+            nlB = selectnode(fens, box=[0.0, 0.0, Ri, Ri, 0.0, Thickness], inflate=tolerance);
+            # thecorneru = zeros(FFlt,length(nlA),3)
+            # gathervalues_asmat!(u, thecorneru, nl);
+            # thecorneru = mean(thecorneru, 1)[1]/phun("mm")
+            # println("displacement = $(thecorneru) vs -0.10215 [MM]")
+
+            println("Extrapolation: $( extrapolation )")
+            sigx = fieldfromintegpoints(femm, geom, u, :Cauchy, 1;
+                tonode = extrapolation)
+            sigy = fieldfromintegpoints(femm, geom, u, :Cauchy, 2;
+                tonode = extrapolation)
+            sigyA = mean(sigy.values[nlA,1], 1)[1]
+            sigyAtrue = sigmatt([Ri, 0.0, 0.0])
+            println("sig_y@A =$(sigyA/phun("MPa")) vs $(sigyAtrue/phun("MPa")) [MPa]")
+            sigxB = mean(sigx.values[nlB,1], 1)[1]
+            sigxBtrue = sigmatt([0.0, Ri, 0.0])
+            println("sig_x@B =$(sigxB/phun("MPa")) vs $(sigxBtrue/phun("MPa")) [MPa]")
+            push!(nelems, count(fes))
+            push!(sigxderrs[extrapolation], abs(sigxB/sigxBtrue - 1.0))
+            push!(sigyderrs[extrapolation], abs(sigyA/sigyAtrue - 1.0))
+            # File =  "a.vtk"
+            # vtkexportmesh(File, fes.conn, geom.values,
+            # FinEtools.MeshExportModule.H8; vectors=[("u", u.values)],
+            # scalars=[("sigmax", sigx.values/phun("MEGA*PA"))])
+            # @async run(`"paraview.exe" $File`)
+        end
+    end
+
+    df = DataFrame(nelems=vec(nelems),
+        sigyderrtrendpaper=vec(sigyderrs[:estimtrendpaper]),
+        sigyderrtrend=vec(sigyderrs[:estimtrend]),
+        sigyderrdefault=vec(sigyderrs[:default]))
+    File = "plate_w_hole_MSH8_convergence.CSV"
+    CSV.write(File, df)
+    @async run(`"paraview.exe" $File`)
+
+end
+end
+using mplate_w_hole_MSH8m
+mplate_w_hole_MSH8m.test()
