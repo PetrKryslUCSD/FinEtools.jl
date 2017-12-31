49ad0ad281a410ae9a4bfbca2889f12b7937a51e
Petr <pkrysl@ucsd.edu>
2017-12-30 19:59:31 -0800

    Fix name of the input file

--- utilities/plotfromCSV.jl
+++ utilities/plotfromCSV.jl
@@ -4,7 +4,7 @@ if VERSION >= v"0.7-"
 end
 
 function loadcsv(inputcsv)
-    contents = readdlm("LE1NAFEMS_MSH8_convergence.CSV", ',', Float64, '\n'; header = true)
+    contents = readdlm(inputcsv, ',', Float64, '\n'; header = true)
     return contents
 end
 
@@ -13,8 +13,8 @@ function coldata(inputcsv, theset)
     return contents[1][:, theset]
 end
 
+# Example
 inputcsv = "LE1NAFEMS_MSH8_convergence.CSV"
-
 
 @pyimport matplotlib.pyplot as plt
 plt.style[:use]("seaborn-whitegrid")
