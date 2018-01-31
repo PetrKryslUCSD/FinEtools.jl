7f973cb30a715089e82fc0ecf1e0e4db0df129c1
Petr <pkrysl@ucsd.edu>
2018-01-30 17:59:45 -0800

    timing

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,21 +33,9 @@
 # INFO: FinEtools tests passed
 
 using Compat.Test
-t0 = time()
-@testset "Miscellaneous" begin include("test_miscellaneous.jl") end
-println("timing = $(time() - t0)")
-t0 = time()
-@testset "Acoustics" begin include("test_acoustics.jl") end
-println("timing = $(time() - t0)")
-t0 = time()
-@testset "Heat diffusion" begin include("test_heat.jl") end
-println("timing = $(time() - t0)")
-t0 = time()
-@testset "Linear deformation" begin include("test_linear_deformation.jl") end
-println("timing = $(time() - t0)")
-t0 = time()
-@testset "Meshing" begin include("test_meshing.jl") end
-println("timing = $(time() - t0)")
-t0 = time()
-@testset "Voxel box" begin include("test_voxel_box.jl") end
-println("timing = $(time() - t0)")
+@time @testset "Miscellaneous" begin include("test_miscellaneous.jl") end
+@time @testset "Acoustics" begin include("test_acoustics.jl") end
+@time @testset "Heat diffusion" begin include("test_heat.jl") end
+@time @testset "Linear deformation" begin include("test_linear_deformation.jl") end
+@time @testset "Meshing" begin include("test_meshing.jl") end
+@time @testset "Voxel box" begin include("test_voxel_box.jl") end
