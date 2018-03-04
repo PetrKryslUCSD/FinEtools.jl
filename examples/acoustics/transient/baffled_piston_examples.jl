03a66d777f930af6733ccbadf2b14914009705fc
Petr <pkrysl@ucsd.edu>
2018-03-03 16:50:43 -0800

    cleanup transient acoustics examples

---
+++ examples/acoustics/transient/baffled_piston_examples.jl
@@ -0,0 +1,188 @@
+module baffled_piston_examples
+using FinEtools
+using LinearAlgebra
+using Gaston
+
+function baffled_piston_H8_transient()
+    rho = 1.21*phun("kg/m^3");# mass density
+c  = 343.0*phun("m/s");# sound speed
+bulk =  c^2*rho;
+frequency=2000; # Hz
+omega=2*pi*frequency;
+P_piston = 1.0e-3*phun("MPa"); # amplitude of the piston pressure
+R = 50.0*phun("mm");# radius of the piston
+Ro = 400.0*phun("mm"); # radius of the enclosure
+nref = 2;#number of refinements of the sphere around the piston
+nlayers = 48;                     # number of layers of elements surrounding the piston
+tolerance = R/(2^nref)/10
+dt=1.0/frequency/20;
+tfinal=50*dt;
+nsteps=round(tfinal/dt)+1;
+
+println("""
+
+Baffled piston in a half-sphere domain with hard boundary condition.
+
+Hexahedral mesh. Algorithm version.
+""")
+
+t0  =  time()
+
+# Hexahedral mesh
+fens,fes  =  H8sphere(R,nref);
+bfes  =  meshboundary(fes)
+# File  =   "baffledabc_boundary.vtk"
+# vtkexportmesh(File, bfes.conn, fens.xyz, FinEtools.MeshExportModule.Q4)
+#  @async run(`"paraview.exe" $File`)
+
+l = selectelem(fens,bfes,facing = true,direction = [1.0 1.0  1.0])
+ex(xyz, layer) = (R+layer/nlayers*(Ro-R))*xyz/norm(xyz)
+fens1,fes1  =  H8extrudeQ4(fens, subset(bfes,l), nlayers, ex);
+fens,newfes1,fes2 =  mergemeshes(fens1, fes1, fens, fes, tolerance)
+fes = cat(newfes1,fes2)
+
+# Piston surface mesh
+bfes  =  meshboundary(fes)
+l1 = selectelem(fens, bfes, facing = true, direction = [-1.0 0.0 0.0])
+l2 = selectelem(fens, bfes, distance = R, from = [0.0 0.0 0.0], inflate = tolerance)
+piston_fes = subset(bfes,intersect(l1,l2));
+# File  =   "piston_fes.vtk"
+# vtkexportmesh(File, piston_fes.conn, fens.xyz, FinEtools.MeshExportModule.Q4)
+#  @async run(`"paraview.exe" $File`)
+
+# Outer spherical boundary
+louter = selectelem(fens, bfes, facing = true, direction = [1.0 1.0  1.0], dotmin= 0.001)
+outer_fes = subset(bfes,louter);
+# File  =   "outer_fes.vtk"
+# vtkexportmesh(File, outer_fes.conn, fens.xyz, FinEtools.MeshExportModule.Q4)
+#  @async run(`"paraview.exe" $File`)
+
+println("Pre-processing time elapsed  =  ",time() - t0,"s")
+
+t1  =  time()
+
+material = MatAcoustFluid(bulk, rho)
+# Region of the fluid
+femm = FEMMAcoust(IntegData(fes, GaussRule(3, 2)), material)
+
+geom = NodalField(fens.xyz);
+P = NodalField(zeros(nnodes(geom),1));
+piston_fenids = connectednodes(piston_fes);
+setebc!(P, piston_fenids, true, 1, 0.0);
+applyebc!(P);
+numberdofs!(P);
+
+S  =  acousticstiffness(femm, geom, P);
+C  =  acousticmass(femm, geom, P);
+D= (2.0/dt)*S +(dt/2.0)*C;
+
+P0 = deepcopy(P)
+Pdd0 = deepcopy(P)
+P1 = deepcopy(P)
+Pdd1 = deepcopy(P)
+TMPF = deepcopy(P)
+P = nothing # we don't need this field anymore
+vP0 = zeros(P0.nfreedofs)
+vP1 = deepcopy(vP0)
+vPd0 = deepcopy(vP0)
+vPd1 = deepcopy(vP0)
+
+t = 0.0
+P0.fixed_values[piston_fenids,1] = P_piston*sin(omega*t)
+Pdd0.fixed_values[piston_fenids,1] = P_piston*(-omega^2)*sin(omega*t)
+vP0 = gathersysvec!(P0, vP0)
+
+nh = selectnode(fens, nearestto = [R+Ro/2, 0.0, 0.0] )
+Pnh = [P1.values[nh, 1][1]]
+ts = [t]
+step =0;
+while t <=tfinal
+    step = step  +1;
+    t=t+dt;
+    P1.fixed_values[piston_fenids,1] = P_piston*sin(omega*t)
+    Pdd1.fixed_values[piston_fenids,1] = P_piston*(-omega^2)*sin(omega*t)
+    TMPF.fixed_values = P0.fixed_values + P1.fixed_values
+    F = nzebcloadsacousticmass(femm, geom, TMPF);
+    TMPF.fixed_values = Pdd0.fixed_values + Pdd1.fixed_values
+    F = F + nzebcloadsacousticstiffness(femm, geom, TMPF);
+    # println("$(norm(F))")
+    vPd1 = D\((2/dt)*(S*vPd0) - C*(2*vP0+(dt/2)*vPd0) + F);
+    vP1 = vP0 + (dt/2)*(vPd0+vPd1);
+    scattersysvec!(P1, vP1); # store current pressure
+    # Swap variables for the next step
+    copyto!(vP0, vP1)
+    copyto!(vPd0, vPd1)
+    copyto!(P0, P1)
+    copyto!(Pdd0, Pdd1)
+    # Graphics output
+    File  =   "baffled_piston-$(step).vtk"
+    vtkexportmesh(File, fes.conn, fens.xyz, FinEtools.MeshExportModule.H8;
+        scalars = [(  "P", P1.values)])
+    #  @async run(`"paraview.exe" $File`)
+    push!(Pnh, P1.values[nh, 1][1])
+    push!(ts, t)
+    println("step = $( step )")
+end
+
+
+
+set(axis="normal", plotstyle="linespoints", linewidth=4, pointsize = 2, color = "black", xlabel = "Time", ylabel = "Pnh", grid="on", title = "")
+f = figure()
+plot(vec(ts), vec(Pnh), legend = "", marker = "edmd")
+figure(f)
+
+# using Plots
+# plotly()
+# plot(vec(ts), vec(Pnh))
+# gui()
+println("$(Pnh[end])")
+
+# # Surface for the ABC
+# abc1  =  FDataDict("femm"=>FEMMAcoustSurf(IntegData(outer_fes, GaussRule(2, 2)),
+#           material))
+#
+# # Surface of the piston
+# flux1  =  FDataDict("femm"=>FEMMAcoustSurf(IntegData(piston_fes, GaussRule(2, 2)),
+#           material),  "normal_flux"=> -rho*a_piston+0.0im);
+#
+# # Make model data
+# modeldata =  FDataDict("fens"=>  fens,
+#                  "omega"=>omega,
+#                  "regions"=>[region1],
+#                  "flux_bcs"=>[flux1], "ABCs"=>[abc1])
+#
+# # Call the solver
+# modeldata = FinEtools.AlgoAcoustModule.steadystate(modeldata)
+#
+# println("Computing time elapsed  =  ",time() - t1,"s")
+# println("Total time elapsed  =  ",time() - t0,"s")
+#
+# geom = modeldata["geom"]
+# P = modeldata["P"]
+#
+# File  =   "baffledabc.vtk"
+# vtkexportmesh(File, fes.conn, geom.values, FinEtools.MeshExportModule.H8;
+# scalars = [("absP", abs.(P.values))])
+# @async run(`"paraview.exe" $File`)
+#
+# # using Winston
+# # pl  =  FramedPlot(title = "Matrix",xlabel = "x",ylabel = "Re P, Im P")
+# # setattr(pl.frame, draw_grid = true)
+# # add(pl, Curve([1:length(C[:])],vec(C[:]), color = "blue"))
+#
+# # # pl = plot(geom.values[nLx,1][ix],scalars[nLx][ix])
+# # # xlabel("x")
+# # # ylabel("Pressure")
+# # display(pl)
+#
+# true
+end # baffled_piston_H8_transient
+
+function allrun()
+    println("#####################################################") 
+    println("# baffled_piston_H8_transient ")
+    baffled_piston_H8_transient()
+    return true
+end # function allrun
+
+end # module baffled_piston_examples
---
+++ examples/acoustics/transient/sphere_dipole_examples.jl
@@ -0,0 +1,170 @@
+module sphere_dipole_examples
+using FinEtools
+using LinearAlgebra
+using Gaston
+
+function sphere_dipole_transient()
+    println("The interior sphere accelerates in the alternately in the positive
+    and negative x-direction, generating positive pressure ahead of it, negative
+    pressure behind. Time-dependent simulation.
+    ")
+    rho = 1.21*phun("kg/m^3");# mass density
+    c  = 343.0*phun("m/s");# sound speed
+    bulk =  c^2*rho;
+    a_amplitude=1.0*phun("mm/s^2");# amplitude of the  acceleration of the sphere
+    # omega = 2000*phun("rev/s");      # frequency of the incident wave
+    R = 50.0*phun("mm"); # radius of the interior sphere
+    Ro = 8*R # radius of the external sphere
+    P_amplitude = R*rho*a_amplitude; # pressure amplitude
+    nref=2;
+    nlayers=40;
+    tolerance = Ro/(nlayers)/100
+    frequency=1200.; # Hz
+    omega=2*pi*frequency;
+    dt=1.0/frequency/20;
+    tfinal=90*dt;
+    nsteps=round(tfinal/dt)+1;
+    
+    println("""
+            Spherical domain filled with acoustic medium, no scatterer.
+    Hexahedral mesh.
+            """)
+    
+    t0  =  time()
+    
+    # Hexahedral mesh
+    fens,fes  =  H8sphere(R,nref);
+    fens1,fes1  =  mirrormesh(fens, fes, [-1.0, 0.0, 0.0], [0.0, 0.0, 0.0],
+    renumb =  r(c) = c[[1, 4, 3, 2, 5, 8, 7, 6]]);
+    fens,newfes1,fes2 =  mergemeshes(fens1, fes1, fens, fes, tolerance)
+    fes = cat(newfes1,fes2)
+    
+    
+    # Derive the finite element sets for the boundary
+    bfes  =  meshboundary(fes)
+    # Outer spherical boundary
+    function dout(xyz)
+        return xyz/norm(xyz)
+    end
+    
+    louter = selectelem(fens, bfes, facing = true, direction = dout)
+    outer_fes = subset(bfes, louter);
+    
+    fens,fes  = H8extrudeQ4(fens, outer_fes, nlayers, (xyz, layer)->(R+layer/nlayers*(Ro-R))*xyz/norm(xyz));
+    connected = findunconnnodes(fens, fes); fens, new_numbering = compactnodes(fens, connected);
+    fess = renumberconn!(fes, new_numbering);
+    
+    geom  =  NodalField(fens.xyz)
+    P  =  NodalField(zeros(FCplxFlt,size(fens.xyz,1),1))
+    
+    bfes  =  meshboundary(fes)
+    
+    # File  =   "Sphere.vtk"
+    # vtkexportmesh(File, bfes.conn, geom.values, FinEtools.MeshExportModule.Q4)
+    # @async run(`"paraview.exe" $File`)
+    
+    # File  =   "Sphere.vtk"
+    # vtkexportmesh (File, fes.conn, fens.xyz, MeshExportModule.H8)
+    # @async run(`"C:/Program Files (x86)/ParaView 4.2.0/bin/paraview.exe" $File`)
+    
+    linner = selectelem(fens, bfes, distance = R, from = [0.0 0.0 0.0], inflate = tolerance)
+    louter = selectelem(fens, bfes, facing = true, direction = dout)
+    
+    println("Pre-processing time elapsed  =  ",time() - t0,"s")
+    
+    t1  =  time()
+    
+    numberdofs!(P)
+    
+    material = MatAcoustFluid(bulk,rho)
+    femm  =  FEMMAcoust(IntegData(fes, GaussRule(3, 2)), material)
+    
+    S  =  acousticstiffness(femm, geom, P);
+    C  =  acousticmass(femm, geom, P);
+    
+    abcfemm  =  FEMMAcoustSurf(IntegData(subset(bfes, louter), GaussRule(2, 2)), material)
+    D  =  acousticABC(abcfemm, geom, P);
+    
+    # Inner sphere pressure loading
+    function dipole(dpdn, xyz, J, label, t)
+        n = cross(J[:,1],J[:,2]);
+        n = vec(n/norm(n));
+        dpdn[1] = -rho*a_amplitude*sin(omega*t)*n[1]
+    end
+    
+    dipfemm  =  FEMMAcoustSurf(IntegData(subset(bfes, linner), GaussRule(2, 2)), material)
+    # F  = distribloads(dipfemm, geom, P, fi, 2);
+    #
+    # K = lufact((1.0+0.0im)*(-omega^2*S + omega*1.0im*D + C)) # We fake a complex matrix here
+    # p = K\F  #
+    #
+    # scattersysvec!(P, p[:])
+    
+    # println(" Minimum/maximum pressure= $(minimum(real(p)))/$(maximum(real(p)))")
+    
+    println("Computing time elapsed  =  ",time() - t1,"s")
+    println("Total time elapsed  =  ",time() - t0,"s")
+    
+    
+    # Solve
+    P0 = deepcopy(P)
+    P0.values[:] = 0.0; # initially all pressure is zero
+    vP0 = gathersysvec(P0);
+    vP1 = zeros(eltype(vP0), size(vP0));
+    vQ0 = zeros(eltype(vP0), size(vP0));
+    vQ1 = zeros(eltype(vP0), size(vP0));
+    t = 0.0;
+    P1 = deepcopy(P0);
+    
+    fi  =  ForceIntensity(FCplxFlt, 1, (dpdn, xyz, J, label)->dipole(dpdn, xyz, J, label, t));
+    La0 = distribloads(dipfemm, geom, P1, fi, 2);
+    
+    A = (2.0/dt)*S + D + (dt/2.)*C;
+    
+    step =0;
+    while t <= tfinal
+      step = step  + 1;
+      println("Time $t ($(step)/$(round(tfinal/dt)+1))")
+      # if graphics
+      #   Camera =1.0e+003 * [-1.4303   -3.0332   -2.0483    0.1553    0.2912    0.2533         0         0    0.0010    0.0066];
+      #   wv = get(P1,'values');
+      #   dcm=data_colormap(struct ('range',P_amplitude*[-1,1], 'colormap',bwr));
+      #   colors=map_data(dcm, wv);
+      #   colorfield = field(struct ('name',['cf'], 'dim', 3, 'data',colors));
+      #   gv=reset (gv,[]);
+      #   camset(gv,Camera);
+      #   draw(bfeb,gv, struct ('x', geom,'u',0*geom,'colorfield',colorfield));
+      #   draw_axes (gv,struct('length',R));
+      #   pause(0.55);
+      # end
+      t = t+dt;
+      fi  =  ForceIntensity(FCplxFlt, 1, (dpdn, xyz, J, label)->dipole(dpdn, xyz, J, label, t));
+      La1 = distribloads(dipfemm, geom, P1, fi, 2);
+      vQ1 = A\((2/dt)*(S*vQ0)-D*vQ0-C*(2*vP0+(dt/2)*vQ0)+La0+La1);
+      vP1 = vP0 + (dt/2)*(vQ0+vQ1);
+      vP0 = vP1;
+      vQ0 = vQ1;
+      P1 = scattersysvec!(P1, vec(vP1));
+      P0 = P1;
+      La0 = La1;
+    end
+    
+    println("$(vP1[1])")
+    
+    File  =   "sphere_dipole_1.vtk"
+    vtkexportmesh(File, fes.conn, geom.values, FinEtools.MeshExportModule.H8;
+     scalars = [( "realP", real(P1.values))])
+    @async run(`"paraview.exe" $File`)
+    
+    
+    true
+end # sphere_dipole_transient
+
+function allrun()
+    println("#####################################################") 
+    println("# sphere_dipole_transient ")
+    sphere_dipole_transient()
+    return true
+end # function allrun
+
+end # module baffled_piston_examples
