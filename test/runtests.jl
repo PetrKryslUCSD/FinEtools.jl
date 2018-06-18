029af49c26e6212ecd89fbeb972a19686184070e
Petr <pkrysl@ucsd.edu>
2018-06-18 07:58:14 -0700

    debug Linux issue

--- test/runtests.jl
+++ test/runtests.jl
@@ -101,9 +101,10 @@
 # [ Info: FinEtools tests passed
 
 using Compat.Test
-@time @testset "Miscellaneous" begin include("test_miscellaneous.jl") end
-@time @testset "Acoustics" begin include("test_acoustics.jl") end
-@time @testset "Heat diffusion" begin include("test_heat.jl") end
-@time @testset "Linear deformation" begin include("test_linear_deformation.jl") end
-@time @testset "Meshing" begin include("test_meshing.jl") end
-@time @testset "Voxel box" begin include("test_voxel_box.jl") end
+@time @testset "Debug" begin include("test_debug.jl") end
+# @time @testset "Miscellaneous" begin include("test_miscellaneous.jl") end
+# @time @testset "Acoustics" begin include("test_acoustics.jl") end
+# @time @testset "Heat diffusion" begin include("test_heat.jl") end
+# @time @testset "Linear deformation" begin include("test_linear_deformation.jl") end
+# @time @testset "Meshing" begin include("test_meshing.jl") end
+# @time @testset "Voxel box" begin include("test_voxel_box.jl") end
---
+++ test/test_debug.jl
@@ -0,0 +1,71 @@
+module mocylpull14
+using FinEtools
+using Compat.Test
+function test()
+    E1=1.0;
+    E2=1.0;
+    E3=3.0;
+    nu12=0.29;
+    nu13=0.29;
+    nu23=0.19;
+    G12=0.3;
+    G13=0.3;
+    G23=0.3;
+    p= 0.15;
+    rin=1.;
+    rex =1.2;
+    Length = 1*rex
+    ua = -0.05*Length
+    tolerance=rin/1000.
+
+    MR = DeforModelRed2DAxisymm
+
+    fens,fes = Q4block(rex-rin,Length,5,20);
+    fens.xyz[:, 1] = fens.xyz[:, 1] .+ rin
+    bdryfes = meshboundary(fes);
+
+    # now we create the geometry and displacement fields
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(size(fens.xyz,1),2)) # displacement field
+
+    # the symmetry plane
+    l1 =selectnode(fens; box=[0 rex 0 0], inflate = tolerance)
+    setebc!(u,l1,true, 2, 0.0)
+    # The other end
+    l1 =selectnode(fens; box=[0 rex Length Length], inflate = tolerance)
+    setebc!(u,l1,true, 2, ua)
+
+    applyebc!(u)
+    numberdofs!(u)
+    # println("Number of degrees of freedom = $(u.nfreedofs)")
+    @test u.nfreedofs == 240
+
+    # Property and material
+    material=MatDeforElastIso(MR, 00.0, E1, nu23, 0.0)
+    # display(material)
+    # println("$(material.D)")
+
+    femm = FEMMDeforLinear(MR, IntegData(fes, GaussRule(2, 2), true), material)
+
+    K =stiffness(femm, geom, u)
+    F = nzebcloadsstiffness(femm, geom, u)
+    U=  K\(F)
+    scattersysvec!(u,U[:])
+
+    fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 1)
+    # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
+    @test abs(minimum(fld.values) - 0.0) < 1.0e-5
+    @test abs(maximum(fld.values) - 0.0) < 1.0e-5
+    fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 2)
+    # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
+    @test abs(minimum(fld.values) - 0.0) < 1.0e-5
+    @test abs(maximum(fld.values) - 0.0) < 1.0e-5
+    fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 3)
+    # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
+    @test abs(minimum(fld.values) - -0.050) < 1.0e-5
+    @test abs(maximum(fld.values) - -0.04999999999999919) < 1.0e-5
+end
+end
+using .mocylpull14
+mocylpull14.test()
+
