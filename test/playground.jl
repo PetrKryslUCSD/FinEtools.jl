e50d081cec17f21dcd9a954a0df0227de8443695
Petr <pkrysl@ucsd.edu>
2018-07-01 02:19:24 -0700

    Playground

--- test/playground.jl
+++ test/playground.jl
@@ -1,87 +1,29 @@
-module mstraight_duct_H8_1
-using FinEtools
-using Compat.Test
-function test()
-    t0  =  time()
+using Test
 
-    rho = 1.21*phun("kg/m^3");# mass density
-    c  = 343.0*phun("m/s");# sound speed
-    bulk =  c^2*rho;
-    omega =  54.5901*phun("rev/s")
-    vn0 =  -1.0*phun("m/s")
-    Lx = 10.0*phun("m");# length of the box, millimeters
-    Ly = 1.0*phun("m"); # length of the box, millimeters
-    n = 20;#number of elements along the length
-
-    # println("""
-    #
-    # Straight duct with anechoic termination.
-    # Example from Boundary element acoustics: Fundamentals and computer codes, TW Wu, page 44.
-    # Both real and imaginary components of the pressure should have amplitude of
-    # rho*c = $(rho*c).
-    #
-    # Hexahedral mesh.
-    # """)
-
-    fens,fes  =  H8block(Lx,Ly,Ly,n,2,2); # Mesh
-    bfes  =  meshboundary(fes)
-    L0 = selectelem(fens,bfes, facing = true, direction = [-1.0 0.0 0.0])
-    L10 = selectelem(fens,bfes, facing = true, direction = [+1.0 0.0 0.0])
-    nLx = selectnode(fens,box = [0.0 Lx  0.0 0.0 0.0 0.0], inflate = Lx/1.0e5)
-
-    geom  =  NodalField(fens.xyz)
-    P  =  NodalField(zeros(Complex128,size(fens.xyz,1),1))
-
-    numberdofs!(P)
+# module mmsparsem1
+# using FinEtools
+# using SparseArrays
+# using Test
+# function test()
+#     N = 10
+#     a = vec(rand(N))
+#     i = convert(Vector{Int64}, round.(rand(N) * N) .+ 1) 
+#     j = convert(Vector{Int64}, round.(rand(N) * N) .+ 1) 
+#     # println("i = $(i)")
+#     # println("j = $(j)")
+#     A = sparse(i, j, a, N+1, N+1)
+#     @test typeof(A * A) <: AbstractSparseMatrix
+#     true
+# end
+# end
+# using .mmsparsem1
+# mmsparsem1.test()
 
 
-    material = MatAcoustFluid(bulk,rho)
-    femm  =  FEMMAcoust(IntegData(fes, GaussRule(3, 2)), material)
+@time @testset "Miscellaneous" begin include("test_miscellaneous.jl") end
+@time @testset "Acoustics" begin include("test_acoustics.jl") end
+@time @testset "Heat diffusion" begin include("test_heat.jl") end
+@time @testset "Linear deformation" begin include("test_linear_deformation.jl") end
+@time @testset "Meshing" begin include("test_meshing.jl") end
+@time @testset "Voxel box" begin include("test_voxel_box.jl") end
 
-    S  =  acousticstiffness(femm, geom, P);
-    C  =  acousticmass(femm, geom, P);
-
-
-    E10femm  =  FEMMAcoustSurf(IntegData(subset(bfes,L10),GaussRule(2, 2)), material)
-    D  =  acousticABC(E10femm, geom, P);
-
-    E0femm  =  FEMMBase(IntegData(subset(bfes,L0), GaussRule(2,  2)))
-    fi  =  ForceIntensity(-1.0im*omega*rho*vn0);
-    F  =  distribloads(E0femm, geom, P, fi, 2);
-
-    p = (-omega^2*S +omega*1.0im*D + C)\F
-    scattersysvec!(P, p[:])
-
-    # println("Pressure amplitude bounds: ")
-    # println("  real $(minimum(real(P.values)))/$(maximum(real(P.values)))")
-    # println("  imag $(minimum(imag(P.values)))/$(maximum(imag(P.values)))")
-    #
-    # println("Total time elapsed  =  ",time() - t0,"s")
-
-    File  =   "straight_duct.vtk"
-    scalars = real(P.values);
-    vtkexportmesh(File, connasarray(fes), geom.values, FinEtools.MeshExportModule.H8;
-    scalars = [("Pressure", scalars)])
-    try rm(File) catch end
-    # @async run(`"paraview.exe" $File`)
-
-    ref=rho*c
-    @test abs(minimum(real(P.values))-(-ref))/ref < 0.02
-    @test abs(minimum(imag(P.values))-(-ref))/ref < 0.02
-    @test abs(maximum(real(P.values))-(+ref))/ref < 0.02
-    @test abs(maximum(imag(P.values))-(+ref))/ref < 0.02
-
-    # plotly()
-    # ix = sortperm(geom.values[nLx,1])
-    # plot(geom.values[nLx,1][ix], real(P.values)[nLx][ix], color = :blue, label = "real")
-    # plot!(geom.values[nLx,1][ix], imag(P.values)[nLx][ix], color = :red, label  =  "imag")
-    # plot!(title = "Straight duct with anechoic termination",
-    # xlabel = "x", ylabel = "Pressure")
-    # gui()
-
-    true
-
-end
-end
-using .mstraight_duct_H8_1
-mstraight_duct_H8_1.test()
