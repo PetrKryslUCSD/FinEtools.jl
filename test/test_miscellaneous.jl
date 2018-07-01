bc85e72cfab1efaac871ca1fe100026ccf3e9455
Petr <pkrysl@ucsd.edu>
2018-07-01 02:18:22 -0700

    Sparse matrix test

--- test/test_miscellaneous.jl
+++ test/test_miscellaneous.jl
@@ -1830,6 +1830,23 @@ end
 using .mPartitioning6mmmmmm
 mPartitioning6mmmmmm.test()
 
-
+module mmsparsem1
+using FinEtools
+using SparseArrays
+using Test
+function test()
+    N = 10
+    a = vec(rand(N))
+    i = convert(Vector{Int64}, round.(rand(N) * N) .+ 1) 
+    j = convert(Vector{Int64}, round.(rand(N) * N) .+ 1) 
+    # println("i = $(i)")
+    # println("j = $(j)")
+    A = sparse(i, j, a, N+1, N+1)
+    @test typeof(A * A) <: AbstractSparseMatrix
+    true
+end
+end
+using .mmsparsem1
+mmsparsem1.test()
 
 
