e8661ef807c4446f21293eaed95da8ce93ec8c6e
Petr <pkrysl@ucsd.edu>
2018-08-09 16:06:38 +0200

    Again?

--- test/test_failing.jl
+++ test/test_failing.jl
@@ -319,3 +319,87 @@ end
 end
 using .mocylpull1
 mocylpull1.test()
+
+module mocylpull13
+using FinEtools
+using Test
+function test()
+    # Cylinder  compressed by enforced displacement, axially symmetric model
+
+
+    # Parameters: make sure this represents isotropic material
+    E1=1.0;
+    E2=E1;
+    E3=E1;
+    nu12=0.29;
+    nu13=nu12;
+    nu23=nu12;
+    G12=E1/2.0/(1.0 + nu12);
+    G13=G12;
+    G23=G12;
+    p= 0.15;
+    rin=1.;
+    rex =1.2;
+    Length = 1*rex
+    ua = -0.05*Length
+    tolerance=rin/1000.
+
+    ##
+    # Note that the FinEtools objects needs to be created with the proper
+    # model-dimension reduction at hand.  In this case that is the axial symmetry
+    # assumption.
+    MR = DeforModelRed2DAxisymm
+
+    fens,fes = Q4block(rex-rin,Length,5,20);
+    fens.xyz[:, 1] = fens.xyz[:, 1] .+ rin
+    bdryfes = meshboundary(fes);
+
+    # now we create the geometry and displacement fields
+    geom = NodalField(fens.xyz)
+    u = NodalField(zeros(size(fens.xyz,1),2)) # displacement field
+
+    # the symmetry plane
+    l1 =selectnode(fens; box=[0 rex 0 0], inflate = tolerance)
+    setebc!(u,l1,true, 2, 0.0)
+    # The other end
+    l1 =selectnode(fens; box=[0 rex Length Length], inflate = tolerance)
+    setebc!(u,l1,true, 2, ua)
+
+    applyebc!(u)
+    numberdofs!(u)
+    # println("Number of degrees of freedom = $(u.nfreedofs)")
+    @test u.nfreedofs == 240
+
+    # Property and material
+    material=MatDeforElastOrtho(MR, E1,E2,E3,nu12,nu13,nu23,G12,G13,G23)
+    # display(material)
+    # println("$(material.D)")
+
+    femm = FEMMDeforLinear(MR, IntegData(fes, GaussRule(2, 2), true), material)
+
+    K =stiffness(femm, geom, u)
+    F = nzebcloadsstiffness(femm, geom, u)
+    U=  K\(F)
+    scattersysvec!(u,U[:])
+
+    fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 1)
+    # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
+    @test abs(minimum(fld.values) - 0.0) < 1.0e-5
+    @test abs(maximum(fld.values) - 0.0) < 1.0e-5
+    fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 2)
+    # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
+    @test abs(minimum(fld.values) - 0.0) < 1.0e-5
+    @test abs(maximum(fld.values) - 0.0) < 1.0e-7
+    fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 3)
+    # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
+    @test abs(minimum(fld.values) - -0.050) < 1.0e-5
+    @test abs(maximum(fld.values) - -0.04999999999999919) < 1.0e-5
+
+    # File =  "mocylpull13.vtk"
+    # vtkexportmesh(File, fens, fes; scalars=[("sigmaz", fld.values)],
+    #               vectors=[("u", u.values)])
+    # @async run(`"paraview.exe" $File`)
+end
+end
+using .mocylpull13
+mocylpull13.test()
