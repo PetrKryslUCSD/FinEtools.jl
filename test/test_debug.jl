fa641db30295b42a18dcd7b15f58694703340f88
Petr <pkrysl@ucsd.edu>
2018-08-09 11:00:28 +0200

    Still there?

--- test/test_debug.jl
+++ test/test_debug.jl
@@ -37,17 +37,17 @@ function test()
     geom = NodalField(fens.xyz)
     u = NodalField(zeros(size(fens.xyz,1),2)) # displacement field
 
-    # the symmetry plane
-    l1 =selectnode(fens; box=[0 rex 0 0], inflate = tolerance)
-    setebc!(u,l1,true, 2, 0.0)
-    # The other end
-    l1 =selectnode(fens; box=[0 rex Length Length], inflate = tolerance)
-    setebc!(u,l1,true, 2, ua)
+    # # the symmetry plane
+    # l1 =selectnode(fens; box=[0 rex 0 0], inflate = tolerance)
+    # setebc!(u,l1,true, 2, 0.0)
+    # # The other end
+    # l1 =selectnode(fens; box=[0 rex Length Length], inflate = tolerance)
+    # setebc!(u,l1,true, 2, ua)
 
-    applyebc!(u)
-    numberdofs!(u)
-    # println("Number of degrees of freedom = $(u.nfreedofs)")
-    @test u.nfreedofs == 240
+    # applyebc!(u)
+    # numberdofs!(u)
+    # # println("Number of degrees of freedom = $(u.nfreedofs)")
+    # @test u.nfreedofs == 240
 
     # Property and material
     material = MatDeforElastIso(MR, 00.0, E1, nu23, 0.0)
@@ -59,28 +59,6 @@ function test()
     @show @code_lowered FEMMDeforLinear(MR, IntegData(fes, GaussRule(2, 2), true), material)
     femm = FEMMDeforLinear(MR, IntegData(fes, GaussRule(2, 2), true), material)
     
-    # K =stiffness(femm, geom, u)
-    # F = nzebcloadsstiffness(femm, geom, u)
-    # U=  K\(F)
-    # scattersysvec!(u,U[:])
-
-    # fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 1)
-    # # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
-    # @test abs(minimum(fld.values) - 0.0) < 1.0e-5
-    # @test abs(maximum(fld.values) - 0.0) < 1.0e-5
-    # fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 2)
-    # # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
-    # @test abs(minimum(fld.values) - 0.0) < 1.0e-5
-    # @test abs(maximum(fld.values) - 0.0) < 1.0e-5
-    # fld= fieldfromintegpoints(femm, geom, u, :princCauchy, 3)
-    # # println("Minimum/maximum = $(minimum(fld.values))/$(maximum(fld.values))")
-    # @test abs(minimum(fld.values) - -0.050) < 1.0e-5
-    # @test abs(maximum(fld.values) - -0.04999999999999919) < 1.0e-5
-
-    # File =  "mocylpull14.vtk"
-    # vtkexportmesh(File, fens, fes; scalars=[("sigmaz", fld.values)],
-    #               vectors=[("u", u.values)])
-    # @async run(`"paraview.exe" $File`)
     true
 end
 end
