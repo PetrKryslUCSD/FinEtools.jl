adb1ba5518fec14151d05d5db6d97e3547c05b5c
Petr <pkrysl@ucsd.edu>
2018-10-29 11:25:42 -0700

    Test Triangle rules

--- test/test_miscellaneous.jl
+++ test/test_miscellaneous.jl
@@ -2112,3 +2112,19 @@ end
 end
 using .mmmBisectm1
 mmmBisectm1.test()
+
+module mmmmtest_trirules
+using FinEtools
+using Test
+import LinearAlgebra: cholesky
+function test()
+    for NPTS = [1, 3, 4, 6, 7, 9, 12, 13]
+        r = TriRule(NPTS)
+        # @show NPTS
+        @test abs(sum(r.weights) - 0.5) <1.e-5
+    end
+    true
+end
+end
+using .mmmmtest_trirules
+mmmmtest_trirules.test()
