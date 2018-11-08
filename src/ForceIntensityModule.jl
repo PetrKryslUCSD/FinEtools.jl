349582f4e3792aa698aac4bdfd96147d18928387
Petr Krysl <pkrysl@ucsd.edu>
2018-11-08 13:49:29 -0800

    Better documentation

--- src/ForceIntensityModule.jl
+++ src/ForceIntensityModule.jl
@@ -15,10 +15,10 @@ Distributed force (force intensity) type.
 The force intensity class. The physical units are
 force per unit volume, where volume depends on to which manifold
 the force is applied:
-force/length^3 (when applied to a 3-D solid),
-force/length^2 (when applied to a surface),
-force/length^1 (when applied along a curve),
-or force/length^0 (when applied at a point).
+- force/length^3 (when applied to a 3-D solid),
+- force/length^2 (when applied to a surface),
+- force/length^1 (when applied along a curve), or 
+- force/length^0 (when applied at a point).
 
 Signature of the function to compute the value of the force  at any given point
 `XYZ`, using the columns of the Jacobian matrix of the element, `tangents`,
@@ -83,7 +83,7 @@ end
 
 Update the force intensity vector.
 
-Returns a vector (stored in the cache).
+Returns a vector (stored in the cache `self.cache`).
 """
 function updateforce!(self::ForceIntensity, XYZ::FFltMat,    tangents::FFltMat, fe_label::FInt)
     return updateretrieve!(self.cache, XYZ, tangents, fe_label)
