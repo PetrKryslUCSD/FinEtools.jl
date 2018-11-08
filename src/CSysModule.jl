f72e7c65317d98a8e7be8babd5536328ed27e8c1
Petr Krysl <pkrysl@ucsd.edu>
2018-11-08 11:01:57 -0800

    Improved documentation

--- src/CSysModule.jl
+++ src/CSysModule.jl
@@ -28,9 +28,11 @@ rotation matrix is given.
 The function signature:
 `update!(csmatout::FFltMat, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)`
 where
-`csmatout`= output matrix buffer, `XYZ`= location  in physical coordinates,
-`tangents`= tangent vector matrix, tangents to the parametric coordinate
-curves  in the element, `fe_label`= finite element label.
+- `csmatout`= output matrix buffer, 
+- `XYZ`= location  in physical coordinates,
+- `tangents`= tangent vector matrix, tangents to the parametric coordinate
+  curves  in the element,
+- `fe_label`= finite element label.
 """
 function CSys(sdim::FInt, mdim::FInt, computecsmat::F) where {F<:Function}
     csmat = fill(zero(FFlt), sdim, mdim); # Allocate buffer, in preparation for the first call
@@ -40,7 +42,7 @@ end
 """
     CSys(csmat::FFltMat)
 
-Construct ccoordinate system when the rotation matrix is given.
+Construct coordinate system when the rotation matrix is given.
 """
 function CSys(csmat::FFltMat)
     function updatebuffer!(csmatout::FFltMat, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
@@ -52,8 +54,7 @@ end
 """
     CSys(dim::FInt)
 
-Construct coordinate system when the
-rotation matrix is the identity.
+Construct coordinate system when the rotation matrix is the identity.
 
 `dim` = is the space dimension.
 """
@@ -67,9 +68,9 @@ end
 Construct coordinate system for isotropic-material used with isoparametric
 finite elements.
 
-`sdim` = number of space dimensions,
-`mdim` = number of manifold dimensions of the finite element in which the coordinate
-system  is being evaluated.
+- `sdim` = number of space dimensions,
+- `mdim` = number of manifold dimensions of the finite element in which the
+  coordinate system  is being evaluated.
 
 # Warning
 If  the coordinate system matrix  should be identity, better use the constructor
@@ -97,7 +98,7 @@ element in which the coordinate system matrix is evaluated,  or perhaps on the
 label `fe_label` of the finite element.
 
 After this function returns, the coordinate system matrix can be retrieved
-from the buffer `self.csmat`.
+from the buffer as `self.csmat`.
 """
 function updatecsmat!(self::CSys, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
     self.updatebuffer!(self.csmat, XYZ, tangents, fe_label)
@@ -107,13 +108,14 @@ end
 """
     gen_iso_csmat(XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
 
-Compute the coordinate system  for an isotropic material using
-information  available  by looking at the coordinate curves of isoparametric
-finite elements.
+Compute the coordinate system  for an isotropic material using information
+available  by looking at the coordinate curves of isoparametric finite
+elements.
 
-`XYZ`= location  in physical coordinates,
-`tangents`= tangent vector matrix, tangents to the parametric coordinate
-curves  in the element, `fe_label`= finite element label.
+- `XYZ`= location  in physical coordinates,
+- `tangents`= tangent vector matrix, tangents to the parametric coordinate
+  curves  in the element, 
+- `fe_label`= finite element label.
 
 The basic assumption here is that the material is isotropic, and
 therefore the choice of the material directions does not really matter as
@@ -121,16 +123,17 @@ long as they correspond to the dimensionality of the element. For
 instance a one-dimensional element (L2 as an example) may be embedded
 in a three-dimensional space.
 
-This function assumes that it is being called for
-an mdim-dimensional manifold element, which is embedded in a
-sdim-dimensional Euclidean space. If mdim == sdim,
-the coordinate system matrix is the identity; otherwise the local
+This function assumes that it is being called for an mdim-dimensional manifold
+element, which is embedded in a sdim-dimensional Euclidean space. If `mdim ==
+sdim`, the coordinate system matrix is the identity; otherwise the local
 coordinate directions are aligned with the linear subspace defined by the
 tangent vectors.
 
-Warning: this *cannot* be reliably used to produce consistent stresses
-because each quadrature point gets a local coordinate system which
-depends on the orientation of the element.
+# Warning 
+
+This *cannot* be reliably used to produce consistent stresses because each
+quadrature point gets a local coordinate system which depends on the
+orientation of the element.
 """
 function gen_iso_csmat!(csmatout::FFltMat,
     XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
