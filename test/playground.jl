bd57d2219d1746fbf92a7a61a5a4fd90559da45d
Petr Krysl <pkrysl@ucsd.edu>
2018-12-11 16:51:52 -0800

    Additional tests for heat flux output

--- test/playground.jl
+++ test/playground.jl
@@ -1,98 +1,216 @@
-# module mmPoiss_heatflux_1
-# using FinEtools
-# using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
-# using Test
-# import LinearAlgebra: cholesky
-# function test()
+module mmblock_heatflux_2
+using FinEtools
+using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
+using Test
+import LinearAlgebra: cholesky, norm
+function test()
+  A = 1.0 # dimension of the domain (length of the side of the square)
+  thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
+  Q = 0.0; # internal heat generation rate
+  function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+    forceout[1] = Q; #heat source
+  end
+  gradtemp = [2.0, -0.5]
+  tempf(x) = (1.0 .+ gradtemp[1] .* x[:,1] .+ gradtemp[2] .* x[:,2]);#the exact distribution of temperature
+  N = 10;# number of subdivisions along the sides of the square domain
+  Rm=[-0.9917568452513019 -0.12813414805267656
+  -0.12813414805267656 0.9917568452513019]
+  Rm=[-0.8020689950104449 -0.5972313850116512
+  -0.5972313850116512 0.8020689950104447]
 
-#   # println("""
-#   #
-#   # Heat conduction example described by Amuthan A. Ramabathiran
-#   # http://www.codeproject.com/Articles/579983/Finite-Element-programming-in-Julia:
-#   # Unit square, with known temperature distribution along the boundary,
-#   # and uniform heat generation rate inside.  Mesh of regular linear TRIANGLES,
-#   # in a grid of 1000 x 1000 edges (2M triangles, 1M degrees of freedom).
-#   # Version: 05/29/2017
-#   # """
-#   # )
-#   t0 = time()
+  fens,fes = T3block(A, A, N, N)
 
-#   A = 1.0 # dimension of the domain (length of the side of the square)
-#   thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
-#   Q = -6.0; # internal heat generation rate
-#   function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
-#     forceout[1] = Q; #heat source
-#   end
-#   tempf(x) = (1.0 .+ x[:,1].^2 .+ 2*x[:,2].^2);#the exact distribution of temperature
-#   N = 10;# number of subdivisions along the sides of the square domain
+  geom = NodalField(fens.xyz)
+  Temp = NodalField(zeros(size(fens.xyz,1),1))
 
-
-#   # println("Mesh generation")
-#   fens,fes = T3block(A, A, N, N)
-
-#   geom = NodalField(fens.xyz)
-#   Temp = NodalField(zeros(size(fens.xyz,1),1))
-
-#   # println("Searching nodes  for BC")
-#   l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
-#   l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
-#   l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
-#   l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
-#   List = vcat(l1, l2, l3, l4)
-#   setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
-#   applyebc!(Temp)
-#   numberdofs!(Temp)
-
-#   t1 = time()
-
-#   material = MatHeatDiff(thermal_conductivity)
-
-#   femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), material)
+  l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
+  l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
+  l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
+  l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
+  List = vcat(l1, l2, l3, l4)
+  setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
+  applyebc!(Temp)
+  numberdofs!(Temp)
 
 
-#   # println("Conductivity")
-#   K = conductivity(femm, geom, Temp)
-#   # println("Nonzero EBC")
-#   F2 = nzebcloadsconductivity(femm, geom, Temp);
-#   # println("Internal heat generation")
-#   # fi = ForceIntensity(FFlt, getsource!);# alternative  specification
-#   fi = ForceIntensity(FFlt[Q]);
-#   F1 = distribloads(femm, geom, Temp, fi, 3);
+  material = MatHeatDiff(thermal_conductivity)
+  femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), CSys(Rm), material)
 
-#   # println("Factorization")
-#   K = cholesky(K)
-#   # println("Solution of the factorized system")
-#   U = K\(F1+F2)
-#   scattersysvec!(Temp,U[:])
+  K = conductivity(femm, geom, Temp)
+  F2 = nzebcloadsconductivity(femm, geom, Temp);
+  fi = ForceIntensity(FFlt[Q]);
+  F1 = distribloads(femm, geom, Temp, fi, 3);
+  U = K\(F1+F2)
+  scattersysvec!(Temp,U[:])
 
-#   # println("Total time elapsed = $(time() - t0) [s]")
-#   # println("Solution time elapsed = $(time() - t1) [s]")
+  trueflux = - vec(thermal_conductivity * gradtemp)
+  fluxerror = 0.0
+  qplocs = []
+  qpfluxes = []
+  function inspector(idat, i, conn, xe, out, loc)
+    qplocs, qpfluxes, trueflux, fluxerror = idat
+    fluxerror  +=  norm(trueflux - vec(out))
+    push!(qplocs, copy(loc))
+    push!(qpfluxes, copy(out))
+    return (qplocs, qpfluxes, trueflux, fluxerror)
+  end
+  idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes, trueflux, fluxerror), :heatflux; :outputcsys=>CSys(2))
 
-#   Error= 0.0
-#   for k=1:size(fens.xyz,1)
-#     Error = Error.+abs.(Temp.values[k,1].-tempf(reshape(fens.xyz[k,:], (1,2))))
-#   end
+  qplocs, qpfluxes, trueflux, fluxerror = idat
+  @test fluxerror / length(qpfluxes) < 1.0e-9
 
-#   @test Error[1]<1.e-5
+  File =  "mmblock_heatflux_2-vectors.vtk"
+  vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
+  File =  "mmblock_heatflux_2.vtk"
+  vtkexportmesh(File, fes.conn, [geom.values 0.0 .* Temp.values], T3; scalars=[("Temperature", Temp.values)])
 
-#   qplocs = []
-#   qpfluxes = []
-#   function inspector(idat, i, conn, xe, out, loc)
-#     qplocs, qpfluxes = idat
-#     push!(qplocs, copy(loc))
-#     push!(qpfluxes, copy(out))
-#     return (qplocs, qpfluxes)
-#   end
-#   idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes), :heatflux)
+  true
+end
+end
+using .mmblock_heatflux_2
+mmblock_heatflux_2.test()
 
-#   File =  "Poiss_heatflux_1-vectors.vtk"
-#   vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
-#   File =  "Poiss_heatflux_1.vtk"
-#   vtkexportmesh(File, fes.conn, [geom.values Temp.values], T3; scalars=[("Temperature", Temp.values)])
+module mmblock_heatflux_3
+using FinEtools
+using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
+using Test
+import LinearAlgebra: cholesky, norm
+function test()
+  A = 1.0 # dimension of the domain (length of the side of the square)
+  thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
+  Q = 0.0; # internal heat generation rate
+  function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+    forceout[1] = Q; #heat source
+  end
+  gradtemp = [2.0, -0.5]
+  tempf(x) = (1.0 .+ gradtemp[1] .* x[:,1] .+ gradtemp[2] .* x[:,2]);#the exact distribution of temperature
+  N = 10;# number of subdivisions along the sides of the square domain
+  Rm=[-0.9917568452513019 -0.12813414805267656
+  -0.12813414805267656 0.9917568452513019]
+  Rm=[-0.8020689950104449 -0.5972313850116512
+  -0.5972313850116512 0.8020689950104447]
 
-#   true
-# end
-# end
-# using .mmPoiss_heatflux_1
-# mmPoiss_heatflux_1.test()
+  fens,fes = T3block(A, A, N, N)
 
+  geom = NodalField(fens.xyz)
+  Temp = NodalField(zeros(size(fens.xyz,1),1))
+
+  l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
+  l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
+  l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
+  l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
+  List = vcat(l1, l2, l3, l4)
+  setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
+  applyebc!(Temp)
+  numberdofs!(Temp)
+
+
+  material = MatHeatDiff(thermal_conductivity)
+  femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), CSys(Rm), material)
+
+  K = conductivity(femm, geom, Temp)
+  F2 = nzebcloadsconductivity(femm, geom, Temp);
+  fi = ForceIntensity(FFlt[Q]);
+  F1 = distribloads(femm, geom, Temp, fi, 3);
+  U = K\(F1+F2)
+  scattersysvec!(Temp,U[:])
+
+  trueflux = transpose(Rm) * (- vec(thermal_conductivity * gradtemp))
+  fluxerror = 0.0
+  qplocs = []
+  qpfluxes = []
+  function inspector(idat, i, conn, xe, out, loc)
+    qplocs, qpfluxes, trueflux, fluxerror = idat
+    fluxerror  +=  norm(trueflux - vec(out))
+    push!(qplocs, copy(loc))
+    push!(qpfluxes, copy(out))
+    return (qplocs, qpfluxes, trueflux, fluxerror)
+  end
+  idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes, trueflux, fluxerror), :heatflux; :outputcsys=>CSys(Rm))
+
+  qplocs, qpfluxes, trueflux, fluxerror = idat
+  @test fluxerror / length(qpfluxes) < 1.0e-9
+
+  File =  "mmblock_heatflux_2-vectors.vtk"
+  vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
+  File =  "mmblock_heatflux_2.vtk"
+  vtkexportmesh(File, fes.conn, [geom.values 0.0 .* Temp.values], T3; scalars=[("Temperature", Temp.values)])
+
+  true
+end
+end
+using .mmblock_heatflux_3
+mmblock_heatflux_3.test()
+
+
+module mmblock_heatflux_4
+using FinEtools
+using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
+using Test
+import LinearAlgebra: cholesky, norm
+function test()
+  A = 1.0 # dimension of the domain (length of the side of the square)
+  thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
+  Q = 0.0; # internal heat generation rate
+  function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+    forceout[1] = Q; #heat source
+  end
+  gradtemp = [2.0, -0.5]
+  tempf(x) = (1.0 .+ gradtemp[1] .* x[:,1] .+ gradtemp[2] .* x[:,2]);#the exact distribution of temperature
+  N = 10;# number of subdivisions along the sides of the square domain
+  outRm=[-0.9917568452513019 -0.12813414805267656
+  -0.12813414805267656 0.9917568452513019]
+  Rm=[-0.8020689950104449 -0.5972313850116512
+  -0.5972313850116512 0.8020689950104447]
+
+  fens,fes = T3block(A, A, N, N)
+
+  geom = NodalField(fens.xyz)
+  Temp = NodalField(zeros(size(fens.xyz,1),1))
+
+  l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
+  l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
+  l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
+  l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
+  List = vcat(l1, l2, l3, l4)
+  setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
+  applyebc!(Temp)
+  numberdofs!(Temp)
+
+
+  material = MatHeatDiff(thermal_conductivity)
+  femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), CSys(Rm), material)
+
+  K = conductivity(femm, geom, Temp)
+  F2 = nzebcloadsconductivity(femm, geom, Temp);
+  fi = ForceIntensity(FFlt[Q]);
+  F1 = distribloads(femm, geom, Temp, fi, 3);
+  U = K\(F1+F2)
+  scattersysvec!(Temp,U[:])
+
+  trueflux = transpose(outRm) * (- vec(thermal_conductivity * gradtemp))
+  fluxerror = 0.0
+  qplocs = []
+  qpfluxes = []
+  function inspector(idat, i, conn, xe, out, loc)
+    qplocs, qpfluxes, trueflux, fluxerror = idat
+    fluxerror  +=  norm(trueflux - vec(out))
+    push!(qplocs, copy(loc))
+    push!(qpfluxes, copy(out))
+    return (qplocs, qpfluxes, trueflux, fluxerror)
+  end
+  idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes, trueflux, fluxerror), :heatflux; :outputcsys=>CSys(outRm))
+
+  qplocs, qpfluxes, trueflux, fluxerror = idat
+  @test fluxerror / length(qpfluxes) < 1.0e-9
+
+  File =  "mmblock_heatflux_2-vectors.vtk"
+  vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
+  File =  "mmblock_heatflux_2.vtk"
+  vtkexportmesh(File, fes.conn, [geom.values 0.0 .* Temp.values], T3; scalars=[("Temperature", Temp.values)])
+
+  true
+end
+end
+using .mmblock_heatflux_4
+mmblock_heatflux_4.test()
--- test/test_heat.jl
+++ test/test_heat.jl
@@ -2625,3 +2625,291 @@ end
 end
 using .mmPoiss_heatflux_1
 mmPoiss_heatflux_1.test()
+
+module mmblock_heatflux_1
+using FinEtools
+using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
+using Test
+import LinearAlgebra: cholesky, norm
+function test()
+  A = 1.0 # dimension of the domain (length of the side of the square)
+  thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
+  Q = 0.0; # internal heat generation rate
+  function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+    forceout[1] = Q; #heat source
+  end
+  gradtemp = [2.0, -0.5]
+  tempf(x) = (1.0 .+ gradtemp[1] .* x[:,1] .+ gradtemp[2] .* x[:,2]);#the exact distribution of temperature
+  N = 10;# number of subdivisions along the sides of the square domain
+
+  fens,fes = T3block(A, A, N, N)
+
+  geom = NodalField(fens.xyz)
+  Temp = NodalField(zeros(size(fens.xyz,1),1))
+
+  l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
+  l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
+  l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
+  l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
+  List = vcat(l1, l2, l3, l4)
+  setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
+  applyebc!(Temp)
+  numberdofs!(Temp)
+
+  material = MatHeatDiff(thermal_conductivity)
+  femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), material)
+
+  K = conductivity(femm, geom, Temp)
+  F2 = nzebcloadsconductivity(femm, geom, Temp);
+  fi = ForceIntensity(FFlt[Q]);
+  F1 = distribloads(femm, geom, Temp, fi, 3);
+  U = K\(F1+F2)
+  scattersysvec!(Temp,U[:])
+
+  trueflux = - vec(thermal_conductivity * gradtemp)
+  fluxerror = 0.0
+  qplocs = []
+  qpfluxes = []
+  function inspector(idat, i, conn, xe, out, loc)
+    qplocs, qpfluxes, trueflux, fluxerror = idat
+    fluxerror  +=  norm(trueflux - vec(out))
+    push!(qplocs, copy(loc))
+    push!(qpfluxes, copy(out))
+    return (qplocs, qpfluxes, trueflux, fluxerror)
+  end
+  idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes, trueflux, fluxerror), :heatflux)
+
+  qplocs, qpfluxes, trueflux, fluxerror = idat
+  @test fluxerror / length(qpfluxes) < 1.0e-9
+
+  File =  "mmblock_heatflux_1-vectors.vtk"
+  vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
+  try rm(File); catch end
+  File =  "mmblock_heatflux_1.vtk"
+  vtkexportmesh(File, fes.conn, [geom.values 0.0 .* Temp.values], T3; scalars=[("Temperature", Temp.values)])
+  try rm(File); catch end
+
+  true
+end
+end
+using .mmblock_heatflux_1
+mmblock_heatflux_1.test()
+
+module mmblock_heatflux_2
+using FinEtools
+using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
+using Test
+import LinearAlgebra: cholesky, norm
+function test()
+  A = 1.0 # dimension of the domain (length of the side of the square)
+  thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
+  Q = 0.0; # internal heat generation rate
+  function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+    forceout[1] = Q; #heat source
+  end
+  gradtemp = [2.0, -0.5]
+  tempf(x) = (1.0 .+ gradtemp[1] .* x[:,1] .+ gradtemp[2] .* x[:,2]);#the exact distribution of temperature
+  N = 10;# number of subdivisions along the sides of the square domain
+  Rm=[-0.9917568452513019 -0.12813414805267656
+  -0.12813414805267656 0.9917568452513019]
+  Rm=[-0.8020689950104449 -0.5972313850116512
+  -0.5972313850116512 0.8020689950104447]
+
+  fens,fes = T3block(A, A, N, N)
+
+  geom = NodalField(fens.xyz)
+  Temp = NodalField(zeros(size(fens.xyz,1),1))
+
+  l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
+  l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
+  l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
+  l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
+  List = vcat(l1, l2, l3, l4)
+  setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
+  applyebc!(Temp)
+  numberdofs!(Temp)
+
+
+  material = MatHeatDiff(thermal_conductivity)
+  femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), CSys(Rm), material)
+
+  K = conductivity(femm, geom, Temp)
+  F2 = nzebcloadsconductivity(femm, geom, Temp);
+  fi = ForceIntensity(FFlt[Q]);
+  F1 = distribloads(femm, geom, Temp, fi, 3);
+  U = K\(F1+F2)
+  scattersysvec!(Temp,U[:])
+
+  trueflux = - vec(thermal_conductivity * gradtemp)
+  fluxerror = 0.0
+  qplocs = []
+  qpfluxes = []
+  function inspector(idat, i, conn, xe, out, loc)
+    qplocs, qpfluxes, trueflux, fluxerror = idat
+    fluxerror  +=  norm(trueflux - vec(out))
+    push!(qplocs, copy(loc))
+    push!(qpfluxes, copy(out))
+    return (qplocs, qpfluxes, trueflux, fluxerror)
+  end
+  idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes, trueflux, fluxerror), :heatflux; :outputcsys=>CSys(2))
+
+  qplocs, qpfluxes, trueflux, fluxerror = idat
+  @test fluxerror / length(qpfluxes) < 1.0e-9
+
+  # File =  "mmblock_heatflux_2-vectors.vtk"
+  # vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
+  # File =  "mmblock_heatflux_2.vtk"
+  # vtkexportmesh(File, fes.conn, [geom.values 0.0 .* Temp.values], T3; scalars=[("Temperature", Temp.values)])
+
+  true
+end
+end
+using .mmblock_heatflux_2
+mmblock_heatflux_2.test()
+
+module mmblock_heatflux_3
+using FinEtools
+using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
+using Test
+import LinearAlgebra: cholesky, norm
+function test()
+  A = 1.0 # dimension of the domain (length of the side of the square)
+  thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
+  Q = 0.0; # internal heat generation rate
+  function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+    forceout[1] = Q; #heat source
+  end
+  gradtemp = [2.0, -0.5]
+  tempf(x) = (1.0 .+ gradtemp[1] .* x[:,1] .+ gradtemp[2] .* x[:,2]);#the exact distribution of temperature
+  N = 10;# number of subdivisions along the sides of the square domain
+  Rm=[-0.9917568452513019 -0.12813414805267656
+  -0.12813414805267656 0.9917568452513019]
+  Rm=[-0.8020689950104449 -0.5972313850116512
+  -0.5972313850116512 0.8020689950104447]
+
+  fens,fes = T3block(A, A, N, N)
+
+  geom = NodalField(fens.xyz)
+  Temp = NodalField(zeros(size(fens.xyz,1),1))
+
+  l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
+  l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
+  l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
+  l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
+  List = vcat(l1, l2, l3, l4)
+  setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
+  applyebc!(Temp)
+  numberdofs!(Temp)
+
+
+  material = MatHeatDiff(thermal_conductivity)
+  femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), CSys(Rm), material)
+
+  K = conductivity(femm, geom, Temp)
+  F2 = nzebcloadsconductivity(femm, geom, Temp);
+  fi = ForceIntensity(FFlt[Q]);
+  F1 = distribloads(femm, geom, Temp, fi, 3);
+  U = K\(F1+F2)
+  scattersysvec!(Temp,U[:])
+
+  trueflux = transpose(Rm) * (- vec(thermal_conductivity * gradtemp))
+  fluxerror = 0.0
+  qplocs = []
+  qpfluxes = []
+  function inspector(idat, i, conn, xe, out, loc)
+    qplocs, qpfluxes, trueflux, fluxerror = idat
+    fluxerror  +=  norm(trueflux - vec(out))
+    push!(qplocs, copy(loc))
+    push!(qpfluxes, copy(out))
+    return (qplocs, qpfluxes, trueflux, fluxerror)
+  end
+  idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes, trueflux, fluxerror), :heatflux; :outputcsys=>CSys(Rm))
+
+  qplocs, qpfluxes, trueflux, fluxerror = idat
+  @test fluxerror / length(qpfluxes) < 1.0e-9
+
+  File =  "mmblock_heatflux_2-vectors.vtk"
+  vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
+  try rm(File); catch end
+  File =  "mmblock_heatflux_2.vtk"
+  vtkexportmesh(File, fes.conn, [geom.values 0.0 .* Temp.values], T3; scalars=[("Temperature", Temp.values)])
+  try rm(File); catch end
+
+  true
+end
+end
+using .mmblock_heatflux_3
+mmblock_heatflux_3.test()
+
+
+module mmblock_heatflux_4
+using FinEtools
+using FinEtools.MeshExportModule: vtkexportmesh, T3, vtkexportvectors
+using Test
+import LinearAlgebra: cholesky, norm
+function test()
+  A = 1.0 # dimension of the domain (length of the side of the square)
+  thermal_conductivity = [i==j ? one(FFlt) : zero(FFlt) for i=1:2, j=1:2]; # conductivity matrix
+  Q = 0.0; # internal heat generation rate
+  function getsource!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+    forceout[1] = Q; #heat source
+  end
+  gradtemp = [2.0, -0.5]
+  tempf(x) = (1.0 .+ gradtemp[1] .* x[:,1] .+ gradtemp[2] .* x[:,2]);#the exact distribution of temperature
+  N = 10;# number of subdivisions along the sides of the square domain
+  outRm=[-0.9917568452513019 -0.12813414805267656
+  -0.12813414805267656 0.9917568452513019]
+  Rm=[-0.8020689950104449 -0.5972313850116512
+  -0.5972313850116512 0.8020689950104447]
+
+  fens,fes = T3block(A, A, N, N)
+
+  geom = NodalField(fens.xyz)
+  Temp = NodalField(zeros(size(fens.xyz,1),1))
+
+  l1 = selectnode(fens; box=[0. 0. 0. A], inflate = 1.0/N/100.0)
+  l2 = selectnode(fens; box=[A A 0. A], inflate = 1.0/N/100.0)
+  l3 = selectnode(fens; box=[0. A 0. 0.], inflate = 1.0/N/100.0)
+  l4 = selectnode(fens; box=[0. A A A], inflate = 1.0/N/100.0)
+  List = vcat(l1, l2, l3, l4)
+  setebc!(Temp, List, true, 1, tempf(geom.values[List,:])[:])
+  applyebc!(Temp)
+  numberdofs!(Temp)
+
+
+  material = MatHeatDiff(thermal_conductivity)
+  femm = FEMMHeatDiff(IntegDomain(fes, TriRule(1), 100.), CSys(Rm), material)
+
+  K = conductivity(femm, geom, Temp)
+  F2 = nzebcloadsconductivity(femm, geom, Temp);
+  fi = ForceIntensity(FFlt[Q]);
+  F1 = distribloads(femm, geom, Temp, fi, 3);
+  U = K\(F1+F2)
+  scattersysvec!(Temp,U[:])
+
+  trueflux = transpose(outRm) * (- vec(thermal_conductivity * gradtemp))
+  fluxerror = 0.0
+  qplocs = []
+  qpfluxes = []
+  function inspector(idat, i, conn, xe, out, loc)
+    qplocs, qpfluxes, trueflux, fluxerror = idat
+    fluxerror  +=  norm(trueflux - vec(out))
+    push!(qplocs, copy(loc))
+    push!(qpfluxes, copy(out))
+    return (qplocs, qpfluxes, trueflux, fluxerror)
+  end
+  idat = inspectintegpoints(femm, geom, Temp, collect(1:count(fes)), inspector, (qplocs, qpfluxes, trueflux, fluxerror), :heatflux; :outputcsys=>CSys(outRm))
+
+  qplocs, qpfluxes, trueflux, fluxerror = idat
+  @test fluxerror / length(qpfluxes) < 1.0e-9
+
+  # File =  "mmblock_heatflux_4-vectors.vtk"
+  # vtkexportvectors(File, qplocs, [("heatflux", qpfluxes)])
+  # File =  "mmblock_heatflux_4.vtk"
+  # vtkexportmesh(File, fes.conn, [geom.values 0.0 .* Temp.values], T3; scalars=[("Temperature", Temp.values)])
+
+  true
+end
+end
+using .mmblock_heatflux_4
+mmblock_heatflux_4.test()
