01ef7d2b018cc2d8d2546b9e76ce2ec6bff8fdf2
Petr Krysl <pkrysl@ucsd.edu>
2019-04-26 08:40:15 -0700

    Update of the assembly module documentation

--- src/AssemblyModule.jl
+++ src/AssemblyModule.jl
@@ -9,6 +9,11 @@ using FinEtools.FTypesModule: FInt, FFlt, FCplxFlt, FFltVec, FIntVec, FFltMat, F
 import SparseArrays: sparse
 import LinearAlgebra: diag
 
+"""
+    SysmatAssemblerBase
+
+Abstract type of system-matrix assembler.
+"""
 abstract type SysmatAssemblerBase end;
 
 mutable struct SysmatAssemblerSparse{T<:Number} <: SysmatAssemblerBase
@@ -21,7 +26,30 @@ mutable struct SysmatAssemblerSparse{T<:Number} <: SysmatAssemblerBase
     ndofs_row::FInt; ndofs_col::FInt;
 end
 
-# AssemblyModule.SysmatAssemblerSparse()
+"""
+    SysmatAssemblerSparse(zero::T=0.0) where {T<:Number}
+
+Construct blank system matrix assembler. The matrix entries are of type `T`.
+
+# Example
+
+This is how a sparse matrix is assembled from two rectangular dense matrices.
+```
+	a = SysmatAssemblerSparse(0.0)                                                        
+	startassembly!(a, 5, 5, 3, 7, 7)    
+	m = [0.24406   0.599773    0.833404  0.0420141                                             
+		0.786024  0.00206713  0.995379  0.780298                                              
+		0.845816  0.198459    0.355149  0.224996]                                     
+	assemble!(a, m, [1 7 5], [5 2 1 4])        
+	m = [0.146618  0.53471   0.614342    0.737833                                              
+		 0.479719  0.41354   0.00760941  0.836455                                              
+		 0.254868  0.476189  0.460794    0.00919633                                            
+		 0.159064  0.261821  0.317078    0.77646                                               
+		 0.643538  0.429817  0.59788     0.958909]                                   
+	assemble!(a, m, [2 3 1 7 5], [6 7 3 4])                                        
+	A = makematrix!(a) 
+```
+"""
 function SysmatAssemblerSparse(zero::T=0.0) where {T<:Number}
     return SysmatAssemblerSparse{T}(0,[zero],[0],[0],0,0,0)
 end
@@ -113,7 +141,7 @@ end
 """
     SysmatAssemblerSparseSymm{T<:Number} <: SysmatAssemblerBase
 
-Assembler for a SYMMETRIC square matrix  assembled from  SYMMETRIC squared
+Assembler for a **symmetric square** matrix  assembled from **symmetric square**
 matrices.
 """
 mutable struct SysmatAssemblerSparseSymm{T<:Number} <: SysmatAssemblerBase
@@ -126,7 +154,33 @@ mutable struct SysmatAssemblerSparseSymm{T<:Number} <: SysmatAssemblerBase
     ndofs:: FInt;
 end
 
-# AssemblyModule.SysmatAssemblerSparse()
+"""
+    SysmatAssemblerSparseSymm(zero::T=0.0) where {T<:Number}
+
+Construct blank system matrix assembler for symmetric matrices. The matrix
+entries are of type `T`.
+
+# Example
+
+This is how a symmetric sparse matrix is assembled from two square dense matrices.
+```
+	a = SysmatAssemblerSparseSymm(0.0)                                                        
+	startassembly!(a, 5, 5, 3, 7, 7)    
+	m = [0.24406   0.599773    0.833404  0.0420141                                             
+		0.786024  0.00206713  0.995379  0.780298                                              
+		0.845816  0.198459    0.355149  0.224996]                                     
+	assemble!(a, m'*m, [5 2 1 4], [5 2 1 4])        
+	m = [0.146618  0.53471   0.614342    0.737833                                              
+		 0.479719  0.41354   0.00760941  0.836455                                              
+		 0.254868  0.476189  0.460794    0.00919633                                            
+		 0.159064  0.261821  0.317078    0.77646                                               
+		 0.643538  0.429817  0.59788     0.958909]                                   
+	assemble!(a, m'*m, [2 3 1 5], [2 3 1 5])                                        
+	A = makematrix!(a) 
+```
+# See also
+[]
+"""
 function SysmatAssemblerSparseSymm(zero::T=0.0) where {T<:Number}
     return SysmatAssemblerSparseSymm{T}(0,[zero],[0],[0],0,0)
 end
@@ -136,7 +190,7 @@ end
       elem_mat_dim::FInt, ignore1::FInt, elem_mat_nmatrices::FInt,
       ndofs::FInt, ignore2::FInt) where {T<:Number}
 
-Start the assembly of a SYMMETRIC square global matrix.
+Start the assembly of a symmetric square global matrix.
 """
 function startassembly!(self::SysmatAssemblerSparseSymm{T},
   elem_mat_dim::FInt, ignore1::FInt, elem_mat_nmatrices::FInt,
@@ -159,10 +213,12 @@ function startassembly!(self::SysmatAssemblerSparseSymm{T},
 end
 
 """
-    assemble!(self::SysmatAssemblerSparseSymm{T}, mat::FMat{T},
-      dofnums::FIntMat, ignore::FIntMat) where {T<:Number}
+    assemble!(self::SysmatAssemblerSparseSymm{T}, mat::FMat{T},  dofnums::FIntVec, ignore::FIntVec) where {T<:Number}
 
 Assemble a square symmetric matrix.
+
+`dofnums` are the row degree of freedom numbers, the column degree of freedom
+number input is ignored (the row and column numbers are the same).
 """
 function assemble!(self::SysmatAssemblerSparseSymm{T}, mat::FMat{T},  dofnums::FIntVec, ignore::FIntVec) where {T<:Number}
     # Assembly of a square symmetric matrix.
@@ -185,10 +241,13 @@ function assemble!(self::SysmatAssemblerSparseSymm{T}, mat::FMat{T},  dofnums::F
 end
 
 """
-assemble!(self::SysmatAssemblerSparseSymm{T}, mat::FMat{T},
-  dofnums::FIntMat, ignore::FIntMat) where {T<:Number}
+	assemble!(self::SysmatAssemblerSparseSymm{T}, mat::FMat{T},
+	  dofnums::FIntMat, ignore::FIntMat) where {T<:Number}
 
 Assemble a square symmetric matrix.
+
+`dofnums` are the row degree of freedom numbers, the column degree of freedom
+number input is ignored (the row and column numbers are the same).
 """
 function assemble!(self::SysmatAssemblerSparseSymm{T}, mat::FMat{T}, dofnums::FIntMat, ignore::FIntMat) where {T<:Number}
     return assemble!(self, mat, vec(dofnums), vec(ignore))
@@ -197,7 +256,7 @@ end
 """
     makematrix!(self::SysmatAssemblerSparseSymm)
 
-Make a sparse SYMMETRIC SQUARE matrix.
+Make a sparse symmetric square matrix.
 """
 function makematrix!(self::SysmatAssemblerSparseSymm)
     # Make a sparse matrix.
@@ -227,7 +286,11 @@ function makematrix!(self::SysmatAssemblerSparseSymm)
 end
 
 
+"""
+    SysvecAssemblerBase
 
+Abstract type of system vector assembler.
+"""
 abstract type SysvecAssemblerBase end;
 
 """
@@ -240,6 +303,11 @@ mutable struct SysvecAssembler{T<:Number} <: SysvecAssemblerBase
     ndofs::FInt
 end
 
+"""
+    SysvecAssembler(zero::T=0.0) where {T<:Number}
+
+Construct blank system vector assembler. The vector entries are of type `T`.
+"""
 function SysvecAssembler(zero::T=0.0) where {T<:Number}
     return SysvecAssembler([zero], 1)
 end
@@ -291,7 +359,7 @@ end
 """
     SysmatAssemblerSparseHRZLumpingSymm{T<:Number} <: SysmatAssemblerBase
 
-Assembler for a SYMMETRIC LUMPED square matrix  assembled from  SYMMETRIC squared
+Assembler for a **symmetric lumped square** matrix  assembled from  **symmetric square**
 matrices. 
 
 Reference: A note on mass lumping and related processes in the finite element method,
@@ -310,7 +378,11 @@ mutable struct SysmatAssemblerSparseHRZLumpingSymm{T<:Number} <: SysmatAssembler
     ndofs:: FInt;
 end
 
-# AssemblyModule.SysmatAssemblerSparse()
+"""
+    SysmatAssemblerSparseHRZLumpingSymm(zer::T=0.0) where {T<:Number}
+
+Construct blank system matrix assembler. The matrix entries are of type `T`.
+"""
 function SysmatAssemblerSparseHRZLumpingSymm(zer::T=0.0) where {T<:Number}
     return SysmatAssemblerSparseHRZLumpingSymm{T}(0,[zer],[0],[0],0,0)
 end
@@ -320,7 +392,7 @@ end
       elem_mat_dim::FInt, ignore1::FInt, elem_mat_nmatrices::FInt,
       ndofs::FInt, ignore2::FInt) where {T<:Number}
 
-Start the assembly of a SYMMETRIC LUMPED square global matrix.
+Start the assembly of a symmetric lumped square global matrix.
 """
 function startassembly!(self::SysmatAssemblerSparseHRZLumpingSymm{T}, elem_mat_dim::FInt, ignore1::FInt, elem_mat_nmatrices::FInt, ndofs::FInt,ignore2::FInt) where {T<:Number}
     # Start the assembly of a global matrix.
@@ -384,7 +456,7 @@ end
 """
     makematrix!(self::SysmatAssemblerSparseHRZLumpingSymm)
 
-Make a sparse SYMMETRIC SQUARE matrix.
+Make a sparse HRZ-lumped **symmetric square**  matrix.
 """
 function makematrix!(self::SysmatAssemblerSparseHRZLumpingSymm)
     # Make a sparse matrix.
