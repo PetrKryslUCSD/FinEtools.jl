f9991233ba5f9ddf34de1ee1d60deafec979e49f
Petr Krysl <pkrysl@ucsd.edu>
2019-04-27 10:29:09 -0700

    Forgot two new modules

---
+++ src/MatModule.jl
@@ -0,0 +1,26 @@
+"""
+    MatModule
+
+Module for abstract material.
+"""
+module MatModule
+
+using FinEtools.FTypesModule: FInt, FFlt, FCplxFlt, FFltVec, FIntVec, FFltMat, FIntMat, FMat, FVec, FDataDict
+
+"""
+    AbstractMat
+
+Abstract type of material.
+"""
+abstract type AbstractMat; end
+
+"""
+    massdensity(self::AbstractMat)
+
+Return mass density.
+"""
+function massdensity(self::AbstractMat)
+	return self.mass_density
+end
+
+end
