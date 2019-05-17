9edf465944f0a37b960f6a6e6662e8a279017df1
Petr Krysl <pkrysl@ucsd.edu>
2019-05-17 09:10:18 -0700

    No modifiable arguments

--- src/DeforModelRedModule.jl
+++ src/DeforModelRedModule.jl
@@ -358,14 +358,10 @@ function Blmat!(MR::Type{DeforModelRed3D}, B::FFltMat, N::FFltMat, gradN::FFltMa
 end
 
 """
-    divmat!(MR::Type{DeforModelRed3D}, B::FFltMat,
-      N::FFltMat, gradN::FFltMat, c::FFltMat, Rm::FFltMat)
+    divmat!(MR::Type{DeforModelRed3D}, N::FFltMat, gradN::FFltMat, c::FFltMat)
 
 Compute the displacement divergence matrix for a three-manifold element.
 
-Compute the displacement divergence matrix for a three-manifold element.
-*The input displacements are in the global Cartesian coordinate system, the
-output strains are in the material coordinate system.*
 
 # Arguments
 - `N` =matrix of basis function values
@@ -385,28 +381,30 @@ output strains are in the material coordinate system.*
   is passed in as a buffer, set to zero,  and filled in  with the nonzero
   components.  It is also returned for convenience.
 """
-function divmat!(MR::Type{DeforModelRed3D}, dimv::FFltMat, N::FFltMat, gradN::FFltMat, c::FFltMat, Rm::FFltMat)
+function divmat!(MR::Type{DeforModelRed3D}, N::FFltMat, gradN::FFltMat, c::FFltMat)
 	nnodes = size(gradN,1);
 	@assert (dim = size(c,2)) == 3;
-	@assert size(dimv) == (1, dim*nnodes)
-	fill!(dimv, 0.0);
-	for i = 1:nnodes
-		k = dim*(i-1);
-		for j = 1:dim
-			dimv[1,k + j] = gradN[i,1]*Rm[j,1] + gradN[i,2]*Rm[j,2] + gradN[i,3]*Rm[j,3]
+	divm = fill(0.0, 1, dim*nnodes)
+	# for i = 1:nnodes
+	# 	k = dim*(i-1);
+	# 	for j = 1:dim
+	# 		divm[1,k + j] = gradN[i,1] + gradN[i,2] + gradN[i,3]
+	# 	end
+	# end
+	for j in 1:dim
+		for i in 1:nnodes
+			k = dim*(i-1);
+			divm[1,k + j] = gradN[i,j]
 		end
 	end
-	return dimv::FFltMat;
+	return divm::FFltMat;
 end
 
 """
-    vgradmat!(MR::Type{DeforModelRed3D}, vgradm::FFltMat,
-      N::FFltMat, gradN::FFltMat, c::FFltMat, Rm::FFltMat)
+    vgradmat!(MR::Type{DeforModelRed3D}, N::FFltMat, gradN::FFltMat, c::FFltMat)
 
 Compute the matrix of displacement gradient in vector form for a three-manifold element.
 
-*The input displacements are in the global Cartesian coordinate system, the
-output strains are in the material coordinate system.*
 
 # Arguments
 - `N` =matrix of basis function values
@@ -426,11 +424,10 @@ output strains are in the material coordinate system.*
   is passed in as a buffer, set to zero,  and filled in  with the nonzero
   components.  It is also returned for convenience.
 """
-function vgradmat!(MR::Type{DeforModelRed3D}, vgradm::FFltMat, N::FFltMat, gradN::FFltMat, c::FFltMat, Rm::FFltMat)
+function vgradmat!(MR::Type{DeforModelRed3D}, N::FFltMat, gradN::FFltMat, c::FFltMat)
 	nnodes = size(gradN,1);
 	@assert (dim = size(c,2)) == 3;
-	@assert size(vgradm) == (dim*dim, dim*nnodes)
-	fill!(vgradm, 0.0);
+	vgradm = fill(0.0, dim*dim, dim*nnodes)
 	for i = 1:dim
 		vgradm[dim*(i-1)+1:dim*i, i:dim:nnodes*dim-dim+i] .= transpose(gradN); 
 	end
