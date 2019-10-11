76277de60a0489e83aa751ad399adf7d6f0285f1
Petr Krysl <pkrysl@ucsd.edu>
2019-10-11 14:41:18 +0200

    Test accessing field values

--- test/playground.jl
+++ test/playground.jl
@@ -1,14 +1,25 @@
-module surfacenormaltest3
+
+module mtfieldtm1
 using FinEtools
 using Test
+using BenchmarkTools
 function test()
-   XYZ = reshape([0.0, 0.0], 2, 1)
-   tangents = reshape([-1.0, 1.0], 2, 1)
-   fe_label = 0
-   fi = SurfaceNormal(2)
-   @show v = updatenormal!(fi, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
-   @test v == [0.7071067811865475, 0.7071067811865475]
+    W = 1.1;
+    L = 12.;
+    t =  0.32;
+    nl, nt, nw = 2, 3, 4;
+
+    fens,fes  = H8block(L,W,t, nl,nw,nt)
+    geom  =  NodalField(fens.xyz)
+
+ecoords = fill(zero(FFlt), nodesperelem(fes), ndofs(geom)); # array of Element coordinates
+@btime gathervalues_asmat!(geom, ecoords, fes.conn[1]);
+
+
+    femm  =  FEMMBase(IntegDomain(fes, GaussRule(3, 2)))
+    V = integratefunction(femm, geom, (x) ->  1.0)
+    @test abs(V - W*L*t)/V < 1.0e-5
 end
 end
-using .surfacenormaltest3
-surfacenormaltest3.test()
+using .mtfieldtm1
+mtfieldtm1.test()
