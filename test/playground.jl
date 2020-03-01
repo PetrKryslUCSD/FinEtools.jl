ec2479806542b30cfff644b51e7c38ba5ce46804
Petr Krysl <pkrysl@ucsd.edu>
2020-03-01 11:00:40 -0800

    export vectors test

--- test/playground.jl
+++ test/playground.jl
@@ -1,15 +1,27 @@
-module msymmx
+
+module mexpvecv2
 using FinEtools
-using FinEtools.MatrixUtilityModule: symmetrize!
-using LinearAlgebra
+using FinEtools.MeshExportModule: VTK, VTK.vtkexportvectors
 using Test
 function test()
-    a = rand(6, 6)
-    b = deepcopy(a)
-    symmetrize!(a)
-    @test norm(a - (b + b')/2) / norm(a) <= 1.0e-9
-    true
+    rin::FFlt, rex::FFlt, nr::FInt, nc::FInt, Angl::FFlt, orientation::Symbol = 100.0, 200.0, 3, 6, pi/3, :a
+    fens, fes = T3annulus(rin::FFlt, rex::FFlt, nr::FInt, nc::FInt, Angl::FFlt, orientation::
+      Symbol)
+    fens.xyz = xyz3(fens)
+    d = (fens.xyz[:, 1].^2 + fens.xyz[:, 2].^2)
+    File = "mesh.vtk"
+    # Export of multiple scalar fields
+    result =  VTK.vtkexportmesh(File, fens, fes; scalars = [("d", d), ("invd", 1 ./ d)])
+    @test result == true
+    # rm(File)
+
+    points = [vec(fens.xyz[idx, :]) for idx in 1:count(fens)] 
+    vectors = [("grad", [vec(fens.xyz[idx, [2,1,3]]) for idx in 1:count(fens)])]
+    filename = "vectors.vtk"
+    result =  VTK.vtkexportvectors(filename, points, vectors)
+    @test result == true
+    # rm(filename)
 end
 end
-using .msymmx
-msymmx.test()
+using .mexpvecv2
+mexpvecv2.test()
