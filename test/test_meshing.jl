32226d50a43c26c281c30665a5678f66132fbec7
Petr Krysl <pkrysl@ucsd.edu>
2020-03-03 16:54:08 -0800

    Q8 annulus test

--- test/test_meshing.jl
+++ test/test_meshing.jl
@@ -4208,3 +4208,27 @@ end
 end
 using .mh8sphere1x2
 mh8sphere1x2.test()
+
+module mq8ann1
+using FinEtools
+using FinEtools.MeshExportModule: VTK, VTK.vtkexportvectors
+using Test
+function test()
+    rin::FFlt, rex::FFlt, nr::FInt, nc::FInt, Angl::FFlt, orientation::Symbol = 100.0, 200.0, 13, 16, pi/3, :a
+    fens, fes = Q8annulus(rin::FFlt, rex::FFlt, nr::FInt, nc::FInt, Angl::FFlt)
+    d = (fens.xyz[:, 1].^2 + fens.xyz[:, 2].^2)
+    File = "mesh.vtk"
+    # Export of multiple scalar fields
+    result =  VTK.vtkexportmesh(File, fens, fes; scalars = [("d", d), ("invd", 1 ./ d)])
+    @test result == true
+    rm(File)
+
+    vectors = [("grad", deepcopy(fens.xyz))]
+    filename = "vectors.vtk"
+    result =  VTK.vtkexportmesh(filename, fens, fes; vectors = vectors)
+    @test result == true
+    rm(filename)
+end
+end
+using .mq8ann1
+mq8ann1.test()
