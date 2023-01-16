08888d94e44d554f3c1a62f6ac84425b807f36d5
Petr Krysl <pkrysl@ucsd.edu>
2023-01-16 08:49:36 -0800

    update documentation for nastran import

--- src/MeshImportModule.jl
+++ src/MeshImportModule.jl
@@ -53,15 +53,17 @@ end
 Import tetrahedral (4- and 10-node) NASTRAN mesh (.nas file).
 
 Limitations:
-1. only the GRID and CTETRA  sections are read.
+1. only the GRID, CTETRA, and PSOLID sections are read.
 2. Only 4-node and 10-node tetrahedra  are handled.
 3. The file should be free-form (data separated by commas). 
 Some fixed-format files can also be processed (large-field, but not small-field).
 
 # Output
 Data dictionary, with keys 
-- "`fens`" (finite element nodes), 
-- "`fesets`" (array of finite element sets).
+- "`fens`": set of finite element nodes, 
+- "`fesets`": array of finite element sets,
+- "`property_ids`": array of property ids (integers) associated with the finite element sets.
+
 """
 function import_NASTRAN(filename; allocationchunk=chunk, expectfixedformat = false)
     lines = readlines(filename)
