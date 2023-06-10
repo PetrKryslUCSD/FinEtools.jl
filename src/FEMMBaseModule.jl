fcd282c8cbe88b64d04046b99e6dfa09d4df60ca
Petr Krysl <pkrysl@ucsd.edu>
2023-06-10 10:04:21 -0700

    update documentation of bilinear forms

--- src/FEMMBaseModule.jl
+++ src/FEMMBaseModule.jl
@@ -1305,6 +1305,9 @@ function (data). Both functions are assumed to be vectors (even if of length
 1). ``f`` is represented with `DataCache`, and needs to return a square
 matrix.
 
+The integral is with respect to the volume of the domain ``V`` (i.e. a three
+dimensional integral).
+
 # Arguments
 - `self` = finite element machine;
 - `assembler` = assembler of the global object;
@@ -1411,16 +1414,20 @@ Compute the sparse matrix implied by the bilinear form of the "diffusion" type.
 \\int_{V}  \\nabla\\vartheta \\cdot c \\cdot \\nabla u \\; \\mathrm{d} V
 ```
 
-Here ``\\nabla\\vartheta`` is the gradient of the test function, ``\\nabla u``
-is the gradient of the trial function, ``c`` is a square matrix of
-coefficients; ``c`` is computed by ``f``, which is a given function(data). Both
-functions are assumed to be vectors (even if of length 1). ``f`` is represented
-with `DataCache`, and needs to return a symmetric square matrix (to represent
-general anisotropic diffusion) or a scalar (to represent isotropic diffusion).
+Here ``\\nabla\\vartheta`` is the gradient of the scalar test function,
+``\\nabla u`` is the gradient of the scalar trial function, ``c`` is a square
+symmetric matrix of coefficients (or a scalar); ``c`` is computed by ``f``,
+which is a given function (data). Both test and trial functions are assumed to
+be from the same approximation space. ``f`` is represented with `DataCache`,
+and needs to return a symmetric square matrix (to represent general anisotropic
+diffusion) or a scalar(to represent isotropic diffusion).
 
 The coefficient matrix ``c`` can be given in the so-called local material
 coordinates: coordinates that are attached to a material point and are
 determined by a local cartesian coordinates system (`mcsys`).
+
+The integral is with respect to the volume of the domain ``V`` (i.e. a three
+dimensional integral).
 
 # Arguments
 - `self` = finite element machine;
