1a9f2e8d7342fd14beff86590af07fc461c9d633
Petr Krysl <pkrysl@ucsd.edu>
2023-06-17 20:21:02 -0700

    check symmetry

--- test/test_forms.jl
+++ test/test_forms.jl
@@ -155,7 +155,7 @@ test()
 nothing
 end
 
-module mdistributedl1
+module mdistributedl1x
 using FinEtools
 using Test
 function test()
@@ -190,8 +190,8 @@ function test()
     true
 end
 end
-using .mdistributedl1
-mdistributedl1.test()
+using .mdistributedl1x
+mdistributedl1x.test()
 
 module mbilform_convection_1
 using FinEtools
@@ -397,6 +397,49 @@ function test()
     G1 = bilform_div_grad(femm, geom, u, DataCache(mu))
     C = diagm([2*mu, 2*mu, 2*mu, mu, mu, mu])
     G2 = bilform_lin_elastic(femm, geom, u, DeforModelRed3D, DataCache(C))
+    # @show v' * G1 * v
+    # @show v' * G2 * v
+    @test abs(v' * G1 * v - v' * G2 * v) / (v' * G1 * v) <= 1.0e-5
+    true
+end
+test()
+nothing
+end
+
+
+module mbilform_div_grad_4
+using FinEtools
+using LinearAlgebra
+using SparseArrays
+using Test
+function test()
+    W = 11.1
+    L = 12.0
+    t = 7.32
+    nl, nt, nw = 12, 33, 24
+    a, b, c, d = (-0.33, 2/3, -1.67, 2/7)
+    mu = 0.13377
+
+    fens, fes = H8block(L, W, t, nl, nw, nt)
+    geom = NodalField(fens.xyz)
+
+    u = NodalField(
+        hcat(
+            reshape([a + b*fens.xyz[j, 1]^2 + c*fens.xyz[j, 2]^3 + d*fens.xyz[j, 3]  for j in eachindex(fens)], count(fens), 1),
+            reshape([b + c*fens.xyz[j, 1] + d*fens.xyz[j, 2]^2 + a*fens.xyz[j, 3]^4  for j in eachindex(fens)], count(fens), 1),
+            reshape([c + d*fens.xyz[j, 1]^3 + a*fens.xyz[j, 2] + b*fens.xyz[j, 3]^2  for j in eachindex(fens)], count(fens), 1)
+            ))
+    numberdofs!(u)
+
+
+    femm = FEMMBase(IntegDomain(fes, GaussRule(3, 2)))
+    v = gathersysvec(u)
+
+    G1 = bilform_div_grad(femm, geom, u, DataCache(mu))
+    @test G1 - G1' == spzeros(size(G1)...)
+    C = diagm([2*mu, 2*mu, 2*mu, mu, mu, mu])
+    G2 = bilform_lin_elastic(femm, geom, u, DeforModelRed3D, DataCache(C))
+    @test G2 - G2' == spzeros(size(G2)...)
     # @show v' * G1 * v
     # @show v' * G2 * v
     @test abs(v' * G1 * v - v' * G2 * v) / (v' * G1 * v) <= 1.0e-5
