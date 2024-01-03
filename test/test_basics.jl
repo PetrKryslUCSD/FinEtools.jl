f30f38b480d3d1ff53128e9de46c77b6546d620d
Petr Krysl <pkrysl@ucsd.edu>
2024-01-03 07:46:31 -0800

    add a few more tests

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -2924,3 +2924,41 @@ function test()
 end
 test()
 end
+
+module m_assembly_x_1
+using FinEtools
+using LinearAlgebra
+using Test
+function test()
+    W = 1.1
+    L = 12.0
+    t = 0.32
+    nl, nt, nw = 2, 3, 4
+
+    fens, fes = H8block(L, W, t, nl, nw, nt)
+    geom = NodalField(fens.xyz)
+    psi = NodalField(fill(1.0, count(fens), 1))
+    nl = collect(1:3)
+    setebc!(psi, nl, true, ones(Int, length(nl)), 0.0)
+    numberdofs!(psi)
+
+    femm = FEMMBase(IntegDomain(fes, GaussRule(3, 2)))
+    v_f = gathersysvec(psi)
+    K = bilform_diffusion(femm, geom, psi, DataCache(Matrix(1.0 * LinearAlgebra.I(3))))
+    K_ff = matrix_blocked_ff(K, nfreedofs(psi))
+    result = abs(v_f' * K_ff * v_f)
+
+    ass = SysmatAssemblerFFBlock(nfreedofs(psi))
+    K_ff1 = bilform_diffusion(femm, ass, geom, psi, DataCache(Matrix(1.0 * LinearAlgebra.I(3))))
+    K_ff2 = bilform_diffusion(femm, ass, geom, psi, DataCache(Matrix(1.0 * LinearAlgebra.I(3))))
+    K_ff3 = bilform_diffusion(femm, ass, geom, psi, DataCache(Matrix(1.0 * LinearAlgebra.I(3))))
+    @test norm(K_ff1 - K_ff2) == 0
+    @test norm(K_ff1 - K_ff3) == 0
+    @test norm(K_ff2 - K_ff3) == 0
+    @test abs(v_f' * K_ff1 * v_f - (result)) / (result) <= 1.0e-5
+    true
+end
+test()
+nothing
+end
+
