23f46793ade60ef9ce61cbcd2cfa18286f17bc1a
Petr Krysl <pkrysl@ucsd.edu>
2024-03-21 14:59:12 -0700

    fix element coloring test

--- test/test_meshing.jl
+++ test/test_meshing.jl
@@ -7097,7 +7097,7 @@ function test()
     
     n2e = FENodeToFEMap(fes.conn, count(fens))
 
-    element_colors = element_coloring(fes, n2e)
+    element_colors, unique_colors = element_coloring(fes, n2e)
 
     @test norm(unique(element_colors) - [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) == 0
 
@@ -7131,7 +7131,7 @@ function test()
 
     n2e = FENodeToFEMap(fes.conn, count(fens))
 
-    element_colors = element_coloring(fes, n2e)
+    element_colors, unique_colors = element_coloring(fes, n2e)
 
     @test norm(unique(element_colors) - [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]) == 0
 
