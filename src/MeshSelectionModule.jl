742a56bf3df07cb199fe51dbfdec2014ba19eca1
Petr Krysl <pkrysl@ucsd.edu>
2024-04-11 16:30:04 -0700

    clarify documentation of selectnode

--- src/MeshSelectionModule.jl
+++ src/MeshSelectionModule.jl
@@ -64,20 +64,20 @@ Select nodes using some criterion.
 ```
 nLx = vselect(fens.xyz, box = [0.0 Lx  0.0 0.0 0.0 0.0], inflate = Lx/1.0e5)
 ```
-
 The keyword 'inflate' may be used to increase or decrease the extent of
 the box (or the distance) to make sure some nodes which would be on the
 boundary are either excluded or included.
 
 ### distance
 ```
-list = selectnode(fens.xyz, distance=1.0+0.1/2^nref, from=[0. 0.],
+list = selectnode(fens.xyz; distance=1.0+0.1/2^nref, from=[0. 0.],
         inflate=tolerance);
 ```
+Find all nodes within a certain distance from a given point.
 
 ### plane
 ```
-candidates = selectnode(fens, plane = [0.0 0.0 1.0 0.0], thickness = h/1000)
+candidates = selectnode(fens; plane = [0.0 0.0 1.0 0.0], thickness = h/1000)
 ```
 The keyword `plane` defines the plane by its normal (the first two or
 three numbers) and its distance from the origin (the last number). Nodes
@@ -86,17 +86,19 @@ distance `thickness` from the plane. The normal is assumed to be of unit
 length, if it isn't apply as such, it will be normalized internally.
 
 ### nearestto
+```
+nh = selectnode(fens; nearestto = [R+Ro/2, 0.0, 0.0] )
+```
 Find the node nearest to the location given.
-```
-nh = selectnode(fens, nearestto = [R+Ro/2, 0.0, 0.0] )
-```
+
 
 
 ### farthestfrom
+```
+nh = selectnode(fens; farthestfrom = [R+Ro/2, 0.0, 0.0] )
+```
 Find the node farthest from the location given.
-```
-nh = selectnode(fens, farthestfroms = [R+Ro/2, 0.0, 0.0] )
-```
+
 """
 function selectnode(fens::FENodeSet; kwargs...)
     nodelist = vselect(fens.xyz; kwargs...)
